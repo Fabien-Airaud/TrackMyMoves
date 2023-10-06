58a4b53dba23342469bdd8e4e7a86c839a873ece
FabienTri <airaudfabien@gmail.com>
2023-10-06 19:32:39 -0400

    Add an empty page view

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ const App = () => {
   return (
     <View style={styles.body}>
       <HeaderBrand />
+      <View style={{height: '80%'}}></View>
       <StatusBar style="auto" />
     </View>
   );
