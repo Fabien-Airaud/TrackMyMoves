9f585fb83e1ff4bd2c46a707ce676a64191ae274
FabienTri <airaudfabien@gmail.com>
2023-10-07 21:17:29 -0400

    Create HomeStack in navigation folder

---
+++ components/navigation/HomeStack.jsx
@@ -0,0 +1,15 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from '../pages/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+};
+
+export default HomeStack;
