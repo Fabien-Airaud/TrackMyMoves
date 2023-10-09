b7a5474161ba8867f40232cad0b36835c5370230
FabienTri <airaudfabien@gmail.com>
2023-10-09 17:01:39 -0400

    Add styles variables and change back button for a log in button

--- components/register/Register.jsx
+++ components/register/Register.jsx
@@ -1,27 +1,35 @@
 import { useTheme } from '@react-navigation/native';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from '@rneui/themed';
 
 import RegisterForm from './RegisterForm';
 
 const Register = ({ navigation }) => {
-    const { colors } = useTheme();
+    // Style variables
+    const { colors, fontSizes } = useTheme();
+    const styles = StyleSheet.create({
+        page: {
+            height: '100%',
+            width: '100%',
+            padding: '5%',
+            alignItems: 'center'
+        },
+        text: {
+            color: colors.text,
+            fontSize: fontSizes.sm,
+            margin: '5%'
+        }
+    });
 
     return (
         <View style={styles.page} >
             <RegisterForm />
-            <Text style={{ color: colors.text, margin: '5%' }}>Register page</Text>
-            <Button title="Go back" onPress={() => navigation.goBack()} style={{ margin: '5%' }} />
+            <Text style={styles.text}>
+                Already registered ?
+                <Button title='Log in' type='clear' onPress={() => navigation.navigate('LogIn')} titleStyle={{ fontSize: fontSizes.sm }} />
+            </Text>
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    page: {
-        height: '100%',
-        width: '100%',
-        padding: '5%',
-        alignItems: 'center'
-    }
-});
 
 export default Register;
