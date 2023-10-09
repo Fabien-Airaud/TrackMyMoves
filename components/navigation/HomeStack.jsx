3a8f8fe1b1b9c80291789e8129c8dd266d562067
FabienTri <airaudfabien@gmail.com>
2023-10-08 22:49:56 -0400

    Add Register page to navigation

--- components/navigation/HomeStack.jsx
+++ components/navigation/HomeStack.jsx
@@ -1,13 +1,15 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from '../homeScreen/HomeScreen';
+import Register from '../register/Register';
 
 const Stack = createNativeStackNavigator();
 
 const HomeStack = () => {
   return (
     <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name='Home' component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name='Register' component={Register} />
     </Stack.Navigator>
   );
 };
