cc900425adbdcb3b7a569f775ae8af81a879cb2e
FabienTri <airaudfabien@gmail.com>
2023-10-11 13:03:53 -0400

    Use global input in register form

--- components/register/RegisterForm.jsx
+++ components/register/RegisterForm.jsx
@@ -5,29 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Checkbox, HelperText, TextInput } from 'react-native-paper';
 import { DatePickerInput } from 'react-native-paper-dates';
 
-const Input = ({ label, placeholder, secureTextEntry, onChangeText, inputMode, right }) => {
-    // Style variables
-    const { colors } = useTheme();
-    const styles = StyleSheet.create({
-        input: {
-            backgroundColor: colors.inputFill,
-            marginVertical: '2%'
-        }
-    });
-
-    return (
-        <TextInput label={label}
-            placeholder={placeholder}
-            secureTextEntry={false || secureTextEntry}
-            onChangeText={onChangeText}
-            inputMode={inputMode}
-            right={right}
-            textColor={colors.text}
-            theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
-            style={styles.input}
-        />
-    );
-};
+import Input from './Input';
 
 const RegisterForm = ({ navigation }) => {
     // Input variables
