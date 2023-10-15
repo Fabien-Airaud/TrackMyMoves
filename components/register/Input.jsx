99b6b762d361582ceb92126df4eecb97eb490858
FabienTri <airaudfabien@gmail.com>
2023-10-15 18:48:00 -0400

    Add an empty helper for each input

--- components/register/Input.jsx
+++ components/register/Input.jsx
@@ -1,8 +1,10 @@
 import { useTheme } from '@react-navigation/native';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { TextInput } from 'react-native-paper';
 
-const Input = ({ label, placeholder, secureTextEntry, onChangeText, inputMode, right }) => {
+import HelperRegister from './HelperRegister';
+
+const Input = ({ label, placeholder, secureTextEntry, onChangeText, inputMode, right, helperType, visible, message }) => {
     // Style variables
     const { colors } = useTheme();
     const styles = StyleSheet.create({
@@ -13,16 +15,19 @@ const Input = ({ label, placeholder, secureTextEntry, onChangeText, inputMode, r
     });
 
     return (
-        <TextInput label={label}
-            placeholder={placeholder}
-            secureTextEntry={false || secureTextEntry}
-            onChangeText={onChangeText}
-            inputMode={inputMode}
-            right={right}
-            textColor={colors.text}
-            theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
-            style={styles.input}
-        />
+        <View>
+            <TextInput label={label}
+                placeholder={placeholder}
+                secureTextEntry={false || secureTextEntry}
+                onChangeText={onChangeText}
+                inputMode={inputMode}
+                right={right}
+                textColor={colors.text}
+                theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
+                style={styles.input}
+            />
+            <HelperRegister helperType={helperType} visible={visible} message={message} />
+        </View>
     );
 };
 
