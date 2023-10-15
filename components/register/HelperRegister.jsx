7c6ffd28111e9e05e17d676fadc52d8e845898de
FabienTri <airaudfabien@gmail.com>
2023-10-15 18:46:32 -0400

    Add default values and change how to display the helper (visible or not)

--- components/register/HelperRegister.jsx
+++ components/register/HelperRegister.jsx
@@ -1,10 +1,10 @@
 import { View } from "react-native";
 import { HelperText } from 'react-native-paper';
 
-const HelperRegister = ({ helperType, visible, message }) => {
+const HelperRegister = ({ helperType = 'error', visible = false, message = '', justifyContent = 'start' }) => {
     return (
-        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-            <HelperText type={helperType} visible={visible} padding='none'>
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: justifyContent }}>
+            <HelperText type={helperType} padding='none' style={{display: visible ? 'flex' : 'none'}}>
                 {message}
             </HelperText>
         </View>
