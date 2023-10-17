5f57b00c76cf03c0c5f012daf5152069645e0a2b
FabienTri <airaudfabien@gmail.com>
2023-10-17 09:48:06 -0400

    Change view in scrollView and add stylesheet

--- components/profile/Profile.jsx
+++ components/profile/Profile.jsx
@@ -1,10 +1,26 @@
-import { Text, View } from "react-native";
+import { useTheme } from '@react-navigation/native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 
 const Profile = () => {
+    // Style variables
+    const { colors, fontSizes } = useTheme();
+    const styles = StyleSheet.create({
+        page: {
+            minHeight: '100%',
+            width: '100%',
+            padding: '5%',
+            backgroundColor: colors.background
+        },
+        text: {
+            color: colors.text,
+            fontSize: fontSizes.sm
+        }
+    });
+
     return (
-        <View>
-            <Text>Profile screen</Text>
-        </View>
+        <ScrollView contentContainerStyle={{ alignItems: 'center' }} style={styles.page}>
+            <Text style={styles.text}> Profile screen </Text>
+        </ScrollView>
     );
 };
 
