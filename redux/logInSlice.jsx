f75e3ffcb9ed3030c4ced5c14a8a329d2b0df3d3
FabienTri <airaudfabien@gmail.com>
2023-10-17 11:34:23 -0400

    Add a default logged in account

--- redux/logInSlice.jsx
+++ redux/logInSlice.jsx
@@ -2,7 +2,20 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const logInSlice = createSlice({
     name: 'logIn',
-    initialState: { account: undefined, rememberMe: false },
+    initialState: {
+        account: {
+            id: '4fbbfd11-4e35-482f-b69d-8fbb3d4f175d',
+            emailAddress: 'airaudfabien@gmail.com',
+            password: 'Fabien2002',
+            firstName: 'Fabien',
+            lastName: 'Airaud',
+            birthdate: '2002-11-26T05:00:00.000Z',
+            height: '192',
+            weight: '63',
+            country: 'Canada'
+        },
+        rememberMe: false
+    },
     reducers: {
         logInAccount: (state, action) => {
             state.account = action.payload.account;
