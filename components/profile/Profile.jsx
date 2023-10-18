09e2b28e96f072437ec1a088d88f28dd166a0eed
FabienTri <airaudfabien@gmail.com>
2023-10-18 11:20:41 -0400

    Add a data element for email address

--- components/profile/Profile.jsx
+++ components/profile/Profile.jsx
@@ -1,25 +1,26 @@
 import { useTheme } from '@react-navigation/native';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux';
+import DataProfile from './DataProfile';
 
 const Profile = () => {
+    // Logged account stored in redux
+    const logAcc = useSelector((state) => state.logIn.account);
+
     // Style variables
-    const { colors, fontSizes } = useTheme();
+    const colors = useTheme();
     const styles = StyleSheet.create({
         page: {
             minHeight: '100%',
             width: '100%',
             padding: '5%',
             backgroundColor: colors.background
-        },
-        text: {
-            color: colors.text,
-            fontSize: fontSizes.sm
         }
     });
 
     return (
         <ScrollView contentContainerStyle={{ alignItems: 'center' }} style={styles.page}>
-            <Text style={styles.text}> Profile screen </Text>
+            <DataProfile label='Email address' data={logAcc.emailAddress} secureTextEntry={false} inputMode='text' />
         </ScrollView>
     );
 };
