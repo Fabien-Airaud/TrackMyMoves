4bc3e3e382dafa6182b9c040fc9cbb544825dd3c
FabienTri <airaudfabien@gmail.com>
2023-10-18 15:18:56 -0400

    Change header for app tab

--- components/navigation/AppTab.jsx
+++ components/navigation/AppTab.jsx
@@ -1,13 +1,16 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import Move from '../move/move';
+import Move from '../move/Move';
 import Profile from '../profile/Profile';
+import { Image } from 'react-native';
+
+const PlaceholderImageSource = require('../../assets/transparent-logo.png');
 
 const Tab = createBottomTabNavigator();
 
 const AppTab = () => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator screenOptions={{headerTitle: 'TrackMyMoves', headerLeft: () => <Image source={PlaceholderImageSource} style={{height: 50, width: 50, marginStart: 10}}/>}}>
             <Tab.Screen name="Move" component={Move} />
             <Tab.Screen name="Profile" component={Profile} />
         </Tab.Navigator>
