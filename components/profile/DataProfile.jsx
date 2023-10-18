3fa7f8cb72df7ac6129b40abdc7b5cbb3594f8d1
FabienTri <airaudfabien@gmail.com>
2023-10-18 14:26:41 -0400

    Add a check and a cancel icon buttons thant disable data edit

--- components/profile/DataProfile.jsx
+++ components/profile/DataProfile.jsx
@@ -1,8 +1,7 @@
 import { useTheme } from '@react-navigation/native';
 import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { IconButton } from 'react-native-paper';
-import { TextInput } from 'react-native-paper';
+import { IconButton, TextInput, List } from 'react-native-paper';
 
 const DataProfile = ({ label, data, secureTextEntry, inputMode }) => {
     // state variables
@@ -16,7 +15,8 @@ const DataProfile = ({ label, data, secureTextEntry, inputMode }) => {
             flexDirection: 'row',
             justifyContent: 'space-between',
             alignItems: 'center',
-            width: '90%',
+            flexWrap: 'wrap',
+            width: '100%',
         },
         text: {
             color: colors.text,
@@ -29,20 +29,30 @@ const DataProfile = ({ label, data, secureTextEntry, inputMode }) => {
     });
 
     return (
-        <View style={styles.view}>
-            <TextInput
-                label={edit ? label : ''}
-                value={value}
-                disabled={!edit}
-                secureTextEntry={false || secureTextEntry}
-                onChangeText={text => setValue(text)}
-                inputMode={inputMode}
-                textColor={colors.text}
-                theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
-                style={styles.input}
-            />
-            <IconButton icon='pencil' onPress={() => setEdit(true)} iconColor={colors.text} containerColor={colors.primary} size={fontSizes.sm} />
-        </View>
+            <View style={styles.view}>
+                <TextInput
+                    label={edit ? label : ''}
+                    value={value}
+                    disabled={!edit}
+                    secureTextEntry={false || secureTextEntry}
+                    onChangeText={text => setValue(text)}
+                    inputMode={inputMode}
+                    textColor={colors.text}
+                    theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
+                    style={styles.input}
+                />
+                <List.Section>
+                    <View style={[styles.view, {paddingHorizontal: 5, minWidth: 3*fontSizes.sm}]}>
+                        {edit ?
+                            <>
+                                <IconButton icon='check-bold' onPress={() => setEdit(false)} iconColor={colors.text} containerColor={colors.success} size={fontSizes.sm} />
+                                <IconButton icon='close-thick' onPress={() => setEdit(false)} iconColor={colors.text} containerColor={colors.error} size={fontSizes.sm} />
+                            </>
+                        :   <IconButton icon='pencil' onPress={() => setEdit(true)} iconColor={colors.text} containerColor={colors.primary} size={fontSizes.sm} />
+                        }
+                    </View>
+                </List.Section>
+            </View>
     );
 };
 
