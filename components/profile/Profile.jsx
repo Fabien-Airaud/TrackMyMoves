08dff35bab004feed929ec728d8500fd2c52bc08
FabienTri <airaudfabien@gmail.com>
2023-10-18 21:17:18 -0400

    Add all data elements but without a dispatch function to modify the data

--- components/profile/Profile.jsx
+++ components/profile/Profile.jsx
@@ -1,4 +1,5 @@
 import { useTheme } from '@react-navigation/native';
+import { useState } from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 import DataProfile from './DataProfile';
@@ -20,7 +21,14 @@ const Profile = () => {
 
     return (
         <ScrollView contentContainerStyle={{ alignItems: 'center' }} style={styles.page}>
-            <DataProfile label='Email address' data={logAcc.emailAddress} secureTextEntry={false} inputMode='text' />
+            <DataProfile label='First name' data={logAcc.firstName} secureTextEntry={false} inputMode='text' />
+            <DataProfile label='Last name' data={logAcc.lastName} secureTextEntry={false} inputMode='text' />
+            <DataProfile label='Email address' data={logAcc.emailAddress} secureTextEntry={false} inputMode='email' />
+            <DataProfile label='Password' data={logAcc.password} secureTextEntry={true} inputMode='text' />
+            <DataProfile label='Birthdate' data={logAcc.birthdate} secureTextEntry={false} inputMode='text' />
+            <DataProfile label='Height (cm)' data={logAcc.height} secureTextEntry={false} inputMode='numeric' />
+            <DataProfile label='Weight (kg)' data={logAcc.weight} secureTextEntry={false} inputMode='decimal' />
+            <DataProfile label='Country' data={logAcc.country} secureTextEntry={false} inputMode='text' />
         </ScrollView>
     );
 };
