4d437fbbda57712540719b51015957253c38feb1
FabienTri <airaudfabien@gmail.com>
2023-10-19 18:45:29 -0400

    Create first name component to update it

---
+++ components/profile/FirstNameProfile.jsx
@@ -0,0 +1,83 @@
+import { useTheme } from '@react-navigation/native';
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { IconButton, TextInput } from 'react-native-paper';
+import { useDispatch } from 'react-redux';
+
+import { updateFirstName } from '../../redux/accountSlice';
+import { updateLogFirstName } from '../../redux/logInSlice';
+
+const FirstNameProfile = ({ id, data }) => {
+    // state variables
+    const [edit, setEdit] = useState(false);
+    const [value, setValue] = useState(data);
+
+    // Style variables
+    const { colors, fontSizes } = useTheme();
+    const styles = StyleSheet.create({
+        view: {
+            flexDirection: 'row',
+            justifyContent: 'start',
+            alignItems: 'center',
+            width: '100%',
+        },
+        text: {
+            color: colors.text,
+            fontSize: fontSizes.sm
+        },
+        input: {
+            width: '75%',
+            backgroundColor: colors.inputFill,
+            marginVertical: '2%',
+            paddingHorizontal: 10
+        }
+    });
+
+    // Dispatch account
+    const dispatch = useDispatch();
+
+    const dispatchFirstName = () => {
+        if (data !== value) {
+            dispatch(
+                updateFirstName({
+                    id: id,
+                    firstName: value
+                })
+            );
+            dispatch(
+                updateLogFirstName({
+                    id: id,
+                    firstName: value
+                })
+            );
+            console.log(data + ' -> ' + value);
+        }
+        setEdit(false);
+    };
+
+    return (
+        <View style={styles.view}>
+            <TextInput
+                label={edit ? 'First Name' : ''}
+                value={value}
+                disabled={!edit}
+                onChangeText={text => setValue(text)}
+                inputMode={'text'}
+                textColor={colors.text}
+                theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
+                style={styles.input}
+            />
+            <View style={[styles.view, { paddingHorizontal: 5, width: 3 * fontSizes.sm }]}>
+                {edit ?
+                    <>
+                        <IconButton icon='check-bold' onPress={() => dispatchFirstName()} iconColor={colors.text} containerColor={colors.success} size={fontSizes.sm} />
+                        <IconButton icon='close-thick' onPress={() => { setEdit(false); setValue(data); }} iconColor={colors.text} containerColor={colors.error} size={fontSizes.sm} />
+                    </>
+                    : <IconButton icon='pencil' onPress={() => setEdit(true)} iconColor={colors.text} containerColor={colors.primary} size={fontSizes.sm} />
+                }
+            </View>
+        </View>
+    );
+};
+
+export default FirstNameProfile;
