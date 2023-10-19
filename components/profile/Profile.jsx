daaff8b464f10c4f718cd4712ac449f20351a6ba
FabienTri <airaudfabien@gmail.com>
2023-10-18 21:54:33 -0400

    Use the new EmailProfile component and see the result of update in a text

--- components/profile/Profile.jsx
+++ components/profile/Profile.jsx
@@ -1,15 +1,16 @@
 import { useTheme } from '@react-navigation/native';
-import { useState } from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 import { useSelector } from 'react-redux';
+
 import DataProfile from './DataProfile';
+import EmailProfile from './EmailProfile';
 
 const Profile = () => {
     // Logged account stored in redux
     const logAcc = useSelector((state) => state.logIn.account);
 
     // Style variables
-    const colors = useTheme();
+    const { colors } = useTheme();
     const styles = StyleSheet.create({
         page: {
             minHeight: '100%',
@@ -23,12 +24,13 @@ const Profile = () => {
         <ScrollView contentContainerStyle={{ alignItems: 'center' }} style={styles.page}>
             <DataProfile label='First name' data={logAcc.firstName} secureTextEntry={false} inputMode='text' />
             <DataProfile label='Last name' data={logAcc.lastName} secureTextEntry={false} inputMode='text' />
-            <DataProfile label='Email address' data={logAcc.emailAddress} secureTextEntry={false} inputMode='email' />
+            <EmailProfile id={logAcc.id} data={logAcc.emailAddress} />
             <DataProfile label='Password' data={logAcc.password} secureTextEntry={true} inputMode='text' />
             <DataProfile label='Birthdate' data={logAcc.birthdate} secureTextEntry={false} inputMode='text' />
             <DataProfile label='Height (cm)' data={logAcc.height} secureTextEntry={false} inputMode='numeric' />
             <DataProfile label='Weight (kg)' data={logAcc.weight} secureTextEntry={false} inputMode='decimal' />
             <DataProfile label='Country' data={logAcc.country} secureTextEntry={false} inputMode='text' />
+            <Text style={{color: colors.text}}> {logAcc.emailAddress} </Text>
         </ScrollView>
     );
 };
