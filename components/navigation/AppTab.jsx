baea71cef3a0fba684664b3390c85dd6a1b6f32e
FabienTri <airaudfabien@gmail.com>
2023-10-20 17:15:48 -0400

    Add a simple logo for move tab

--- components/navigation/AppTab.jsx
+++ components/navigation/AppTab.jsx
@@ -9,6 +9,12 @@ const PlaceholderImageSource = require('../../assets/transparent-logo.png');
 
 const Tab = createBottomTabNavigator();
 
+const MoveIcone = ({ focused, color, size }) => {
+    return (
+        <IconButton icon='map-marker-plus' focused={focused} iconColor={color} size={size} />
+    );
+};
+
 const ProfileIcone = ({ focused, color, size }) => {
     return (
         <IconButton icon='account-edit' focused={focused} iconColor={color} size={size} />
@@ -18,7 +24,7 @@ const ProfileIcone = ({ focused, color, size }) => {
 const AppTab = () => {
     return (
         <Tab.Navigator screenOptions={{ headerTitle: 'TrackMyMoves', headerLeft: () => <Image source={PlaceholderImageSource} style={{ height: 50, width: 50, marginStart: 10 }} /> }}>
-            <Tab.Screen name="Move" component={Move} />
+            <Tab.Screen name="Move" component={Move} options={{ tabBarIcon: MoveIcone }} />
             <Tab.Screen name="Profile" component={Profile} options={{ tabBarIcon: ProfileIcone }} />
         </Tab.Navigator>
     );
