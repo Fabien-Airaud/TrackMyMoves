80f6ee3c75db0fb70c3cf0dadc83bcfc2ae255c9
FabienTri <airaudfabien@gmail.com>
2023-10-20 17:14:21 -0400

    Add a simple logo to profile tab

--- components/navigation/AppTab.jsx
+++ components/navigation/AppTab.jsx
@@ -1,18 +1,25 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { IconButton } from 'react-native-paper';
 
+import { Image } from 'react-native';
 import Move from '../move/Move';
 import Profile from '../profile/Profile';
-import { Image } from 'react-native';
 
 const PlaceholderImageSource = require('../../assets/transparent-logo.png');
 
 const Tab = createBottomTabNavigator();
 
+const ProfileIcone = ({ focused, color, size }) => {
+    return (
+        <IconButton icon='account-edit' focused={focused} iconColor={color} size={size} />
+    );
+};
+
 const AppTab = () => {
     return (
-        <Tab.Navigator screenOptions={{headerTitle: 'TrackMyMoves', headerLeft: () => <Image source={PlaceholderImageSource} style={{height: 50, width: 50, marginStart: 10}}/>}}>
+        <Tab.Navigator screenOptions={{ headerTitle: 'TrackMyMoves', headerLeft: () => <Image source={PlaceholderImageSource} style={{ height: 50, width: 50, marginStart: 10 }} /> }}>
             <Tab.Screen name="Move" component={Move} />
-            <Tab.Screen name="Profile" component={Profile} />
+            <Tab.Screen name="Profile" component={Profile} options={{ tabBarIcon: ProfileIcone }} />
         </Tab.Navigator>
     );
 };
