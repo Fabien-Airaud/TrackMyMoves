5a02634b66e47fc0e8e21ad76b5d652dc2b2ce07
FabienTri <airaudfabien@gmail.com>
2023-10-21 23:41:57 -0400

    Use dropdown with a test list

--- components/move/TopMove.jsx
+++ components/move/TopMove.jsx
@@ -1,7 +1,34 @@
 import { useTheme } from '@react-navigation/native';
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import DropDown from 'react-native-paper-dropdown';
+
+import Dropdown from '../Dropdown';
 
 const TopMove = () => {
+    // Variables
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    // Activity dropdown list
+    const items = [
+        {
+            label: 'Item 1',
+            value: 'Item1'
+        },
+        {
+            label: 'Item 2',
+            value: 'Item2'
+        },
+        {
+            label: 'Item 3',
+            value: 'Item3'
+        },
+        {
+            label: 'Item 4',
+            value: 'Item4'
+        }
+    ]
+
     // Style variables
     const { colors, fontSizes } = useTheme();
     const styles = StyleSheet.create({
@@ -12,13 +39,21 @@ const TopMove = () => {
             justifyContent: 'center'
         },
         warningText: {
-            color: colors.error
+            color: colors.error,
+            textAlign: 'center'
+        },
+        container: {
+            marginTop: 10
+        },
+        dropdown: {
+            margin: 10
         }
     });
 
     return (
         <View style={styles.section}>
             <Text style={styles.warningText}> Please note, the type of activity can no longer be modified after the activity has started </Text>
+            <Dropdown width={120} containerStyle={styles.container} items={items} />
         </View>
     );
 };
