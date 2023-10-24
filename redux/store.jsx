2e92b481503faf16867a3aa7ccc26ef91e2d02ac
FabienTri <airaudfabien@gmail.com>
2023-10-24 09:52:24 -0400

    Add activity slice to the store

--- redux/store.jsx
+++ redux/store.jsx
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+
 import accountReducer from './accountSlice';
 import logInReducer from './logInSlice';
+import activitySlice from './activitySlice';
 
 export default configureStore({
 	reducer: {
 		accounts: accountReducer,
-		logIn: logInReducer
+		logIn: logInReducer,
+		activities: activitySlice
 	}
 });
