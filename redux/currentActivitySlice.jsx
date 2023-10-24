b328a631e0e112f96658b06127d1d28647bee32e
FabienTri <airaudfabien@gmail.com>
2023-10-24 11:59:02 -0400

    Add stop action

--- redux/currentActivitySlice.jsx
+++ redux/currentActivitySlice.jsx
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { v4 } from 'uuid';
 
 export const currentActivitySlice = createSlice({
     name: 'currentActivity',
@@ -6,19 +7,23 @@ export const currentActivitySlice = createSlice({
     reducers: {
         createActivity: (state, action) => {
             return {
-                id: action.payload.id,
+                id: v4(),
                 accountId: action.payload.accountId,
                 type: action.payload.activityType,
                 startDate: action.payload.startDate,
                 endDate: ''
             };
+        },
+        stopActivity: (state, action) => {
+            state.endDate = action.payload.endDate;
         }
     }
 });
 
 
 export const {
-    createActivity
+    createActivity,
+    stopActivity
 } = currentActivitySlice.actions;
 
 export default currentActivitySlice.reducer;
