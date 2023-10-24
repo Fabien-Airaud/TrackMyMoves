5bf70be545c67224a8dc9cb9eb3563cd1f269311
FabienTri <airaudfabien@gmail.com>
2023-10-24 10:44:51 -0400

    Change display when start or stop page

--- components/move/BottomMove.jsx
+++ components/move/BottomMove.jsx
@@ -1,7 +1,9 @@
 import { useTheme } from '@react-navigation/native';
 import { StyleSheet, Text, View } from "react-native";
 
-const BottomMove = () => {
+import { MovePageType } from './Move';
+
+const BottomMove = ({ pageType }) => {
     // Style variables
     const { colors, fontSizes } = useTheme();
     const styles = StyleSheet.create({
@@ -13,11 +15,24 @@ const BottomMove = () => {
         }
     });
 
-    return (
-        <View style={styles.section}>
-            <Text> Bottom </Text>
-        </View>
-    );
+    switch (pageType) {
+        case MovePageType.start:
+            return (
+                <View style={styles.section}>
+                    <Text> Start bottom </Text>
+                </View>
+            );
+
+        case MovePageType.stop:
+            return (
+                <View style={styles.section}>
+                    <Text> Stop bottom </Text>
+                </View>
+            );
+
+        default:
+            break;
+    };
 };
 
 export default BottomMove;
