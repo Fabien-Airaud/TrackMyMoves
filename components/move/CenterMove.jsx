93cdace07cd3ce75df2c376af44c79ba5c51e7d0
FabienTri <airaudfabien@gmail.com>
2023-10-24 11:47:03 -0400

    Dispatch the current activity to the store when button start pressed

--- components/move/CenterMove.jsx
+++ components/move/CenterMove.jsx
@@ -5,13 +5,10 @@ import { IconButton } from 'react-native-paper';
 import { useDispatch, useSelector } from 'react-redux';
 import { v4 } from 'uuid';
 
-import { createActivity } from '../../redux/activitySlice';
+import { createActivity } from '../../redux/currentActivitySlice';
 import { MovePageType } from './Move';
 
 const CenterMove = ({ activityType, pageType, setPageType }) => {
-    // Current activity
-    let activity = undefined;
-
     // Style variables
     const { colors, fontSizes } = useTheme();
     const styles = StyleSheet.create({
@@ -30,26 +27,37 @@ const CenterMove = ({ activityType, pageType, setPageType }) => {
     // Logged account stored in redux
     const logAcc = useSelector((state) => state.logIn.account);
 
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
     // Create a new activity when start button pressed and change to stop move page
-    const startActivity = () => {
-        activity = {
-            id: v4(),
-            accountId: logAcc.id,
-            type: activityType,
-            startDate: new Date(),
-            endDate: undefined 
-        }
+    const dispatchNewActivity = () => {
+        const startDate = new Date();
+
+        dispatch(
+            createActivity({
+                id: v4(),
+                accountId: logAcc.id,
+                type: activityType,
+                startDate: startDate.toISOString(),
+                endDate: ''
+            })
+        );
 
         setPageType(MovePageType.stop);
+    };
+
+    // Change endDate of the current activity
+    const stopActivity = () => {
+        activity.endDate = new Date();
+        console.log(activity);
+        // setPageType(MovePageType.save);
     };
 
     switch (pageType) {
         case MovePageType.start:
             return (
                 <View style={styles.section}>
-                    <IconButton disabled={activityType == undefined} onPress={startActivity} icon='play-circle-outline' iconColor={colors.primary} size={fontSizes.bigButton} />
+                    <IconButton disabled={activityType == undefined} onPress={dispatchNewActivity} icon='play-circle-outline' iconColor={colors.primary} size={fontSizes.bigButton} />
                     <Text style={styles.textButton}> Start </Text>
                 </View>
             );
@@ -57,7 +65,7 @@ const CenterMove = ({ activityType, pageType, setPageType }) => {
         case MovePageType.stop:
             return (
                 <View style={styles.section}>
-                    <IconButton icon='stop-circle-outline' iconColor={colors.primary} size={fontSizes.bigButton} />
+                    <IconButton onPress={stopActivity} icon='stop-circle-outline' iconColor={colors.primary} size={fontSizes.bigButton} />
                     <Text style={styles.textButton}> Stop </Text>
                 </View>
             );
