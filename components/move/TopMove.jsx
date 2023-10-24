e6e0d18f7dad6810f08bdfb4d102384ec95bad71
FabienTri <airaudfabien@gmail.com>
2023-10-24 10:42:39 -0400

    Change display when stop page

--- components/move/TopMove.jsx
+++ components/move/TopMove.jsx
@@ -1,9 +1,10 @@
 import { useTheme } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { MovePageType } from './Move';
 import Dropdown from '../Dropdown';
 
-const TopMove = ({ activityType, setActivityType }) => {
+const TopMove = ({ activityType, setActivityType, pageType }) => {
     // Activity dropdown list
     const activityTypes = [
         {
@@ -28,7 +29,7 @@ const TopMove = ({ activityType, setActivityType }) => {
     ]
 
     // Style variables
-    const { colors } = useTheme();
+    const { colors, fontSizes } = useTheme();
     const styles = StyleSheet.create({
         section: {
             width: '80%',
@@ -42,15 +43,35 @@ const TopMove = ({ activityType, setActivityType }) => {
         },
         container: {
             marginTop: 10
+        },
+        activityText: {
+            color: colors.text,
+            fontSize: fontSizes.lg,
+            textAlign: 'center'
         }
     });
 
-    return (
-        <View style={styles.section}>
-            <Text style={styles.warningText}> Please note, the type of activity can no longer be modified after the activity has started </Text>
-            <Dropdown items={activityTypes} value={activityType} setValue={setActivityType} leadingIcon={true} width={120} containerStyle={styles.container} />
-        </View>
-    );
+    switch (pageType) {
+        case MovePageType.start:
+            return (
+                <View style={styles.section}>
+                    <Text style={styles.warningText}> Please note, the type of activity can no longer be modified after the activity has started </Text>
+                    <Dropdown items={activityTypes} value={activityType} setValue={setActivityType} leadingIcon={true} width={120} containerStyle={styles.container} />
+                </View>
+            );
+
+        case MovePageType.stop:
+            return (
+                <View style={styles.section}>
+                    <Text style={styles.activityText}>Stop move page</Text>
+                </View>
+            );
+
+        default:
+            break;
+    }
+
+    
 };
 
 export default TopMove;
