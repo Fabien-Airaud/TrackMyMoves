34a79509b5d702a13767a7d39fce5f852938a54a
FabienTri <airaudfabien@gmail.com>
2023-10-24 12:00:01 -0400

    Dispatch stop activity and remove use of v4 for id

--- components/move/CenterMove.jsx
+++ components/move/CenterMove.jsx
@@ -1,11 +1,9 @@
 import { useTheme } from '@react-navigation/native';
 import { StyleSheet, Text, View } from "react-native";
-import 'react-native-get-random-values';
 import { IconButton } from 'react-native-paper';
 import { useDispatch, useSelector } from 'react-redux';
-import { v4 } from 'uuid';
 
-import { createActivity } from '../../redux/currentActivitySlice';
+import { createActivity, stopActivity } from '../../redux/currentActivitySlice';
 import { MovePageType } from './Move';
 
 const CenterMove = ({ activityType, pageType, setPageType }) => {
@@ -35,7 +33,6 @@ const CenterMove = ({ activityType, pageType, setPageType }) => {
 
         dispatch(
             createActivity({
-                id: v4(),
                 accountId: logAcc.id,
                 type: activityType,
                 startDate: startDate.toISOString(),
@@ -47,10 +44,14 @@ const CenterMove = ({ activityType, pageType, setPageType }) => {
     };
 
     // Change endDate of the current activity
-    const stopActivity = () => {
-        activity.endDate = new Date();
-        console.log(activity);
-        // setPageType(MovePageType.save);
+    const dispatchStopActivity = () => {
+        const endDate = new Date();
+
+        dispatch(
+            stopActivity({ endDate: endDate.toISOString() })
+        );
+
+        setPageType(MovePageType.save);
     };
 
     switch (pageType) {
@@ -65,7 +66,7 @@ const CenterMove = ({ activityType, pageType, setPageType }) => {
         case MovePageType.stop:
             return (
                 <View style={styles.section}>
-                    <IconButton onPress={stopActivity} icon='stop-circle-outline' iconColor={colors.primary} size={fontSizes.bigButton} />
+                    <IconButton onPress={dispatchStopActivity} icon='stop-circle-outline' iconColor={colors.primary} size={fontSizes.bigButton} />
                     <Text style={styles.textButton}> Stop </Text>
                 </View>
             );
