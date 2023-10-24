ff20c3cf1b98399245189bc5856359e1f5f37d1e
FabienTri <airaudfabien@gmail.com>
2023-10-24 19:49:54 -0400

    Create a function that returns the time in a formatted string

---
+++ components/move/FormatTime.jsx
@@ -0,0 +1,18 @@
+/**
+ * Format the time received in a string to display
+ * @param {number} time - The time to format, in seconds
+ * @returns {string} a string representing the time in hours, minutes and seconds
+ */
+export const FormatTime = (time) => {
+    // Time in seperated variables
+    const hours = Math.floor(time / 60 / 60);
+    const mins = Math.floor((time / 60) % 60);
+    const seconds = Math.floor(time % 60);
+
+    // Time in string variables
+    const stringHours = hours < 10 ? `0${hours}` : `${hours}`
+    const stringMins = mins < 10 ? `0${mins}` : `${mins}`
+    const stringSecs = seconds < 10 ? `0${seconds}` : `${seconds}`
+
+    return `${stringHours} : ${stringMins} : ${stringSecs}`;
+}
