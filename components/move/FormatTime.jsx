d07cec92b0464df5942a7867bca1b65a23703220
FabienTri <airaudfabien@gmail.com>
2023-10-24 20:21:37 -0400

    Correct time to be in milliseconds

--- components/move/FormatTime.jsx
+++ components/move/FormatTime.jsx
@@ -1,13 +1,13 @@
 /**
  * Format the time received in a string to display
- * @param {number} time - The time to format, in seconds
+ * @param {number} time - The time to format, in milliseconds
  * @returns {string} a string representing the time in hours, minutes and seconds
  */
 export const formatTime = (time) => {
     // Time in seperated variables
-    const hours = Math.floor(time / 60 / 60);
-    const mins = Math.floor((time / 60) % 60);
-    const seconds = Math.floor(time % 60);
+    const hours = Math.floor(time / 1000 / 60 / 60);
+    const mins = Math.floor((time / 1000 / 60) % 60);
+    const seconds = Math.floor(time / 1000 % 60);
 
     // Time in string variables
     const stringHours = hours < 10 ? `0${hours}` : `${hours}`
