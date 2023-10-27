6898dba02fd7783b7ee377fafa8f1d22d67a80fa
FabienTri <airaudfabien@gmail.com>
2023-10-27 16:15:23 -0400

    Add temp page to test accelerometer

--- components/navigation/AppTab.jsx
+++ components/navigation/AppTab.jsx
@@ -4,6 +4,7 @@ import { IconButton } from 'react-native-paper';
 import { Image } from 'react-native';
 import Move from '../move/Move';
 import Profile from '../profile/Profile';
+import SensorAccelerometer from '../sensors/SensorAccelerometer';
 
 const PlaceholderImageSource = require('../../assets/transparent-logo.png');
 
@@ -12,6 +13,12 @@ const Tab = createBottomTabNavigator();
 const MoveIcone = ({ focused, color, size }) => {
     return (
         <IconButton icon='map-marker-plus' focused={focused} iconColor={color} size={size} />
+    );
+};
+
+const Sensors = ({ focused, color, size }) => {
+    return (
+        <IconButton icon='motion-sensor' focused={focused} iconColor={color} size={size} />
     );
 };
 
@@ -25,6 +32,7 @@ const AppTab = () => {
     return (
         <Tab.Navigator screenOptions={{ headerTitle: 'TrackMyMoves', headerLeft: () => <Image source={PlaceholderImageSource} style={{ height: 50, width: 50, marginStart: 10 }} /> }}>
             <Tab.Screen name="Move" component={Move} options={{ tabBarIcon: MoveIcone }} />
+            <Tab.Screen name="Sensors" component={SensorAccelerometer} options={{ tabBarIcon: Sensors }} />
             <Tab.Screen name="Profile" component={Profile} options={{ tabBarIcon: ProfileIcone }} />
         </Tab.Navigator>
     );
