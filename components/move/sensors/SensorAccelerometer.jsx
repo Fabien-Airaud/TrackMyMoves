5eb13e1554a120b80bc08ff33ae0c5a1d52129f5
FabienTri <airaudfabien@gmail.com>
2023-10-27 23:15:12 -0400

    Add function to subscribe or unsubscribe and create interval

--- components/move/sensors/SensorAccelerometer.jsx
+++ components/move/sensors/SensorAccelerometer.jsx
@@ -3,30 +3,52 @@ import { useEffect, useState } from 'react';
 
 import { TimerStatus } from '../Move';
 
-const SensorAccelerometer = ({timerStatus}) => {
-    const [{ x, y, z }, setData] = useState({
-        x: 0,
-        y: 0,
-        z: 0,
+const SensorAccelerometer = ({ timerStatus }) => {
+    const [accelInter, setAccelInter] = useState({
+        startDate: '',
+        interval: []
     });
     const [subscription, setSubscription] = useState(null);
 
-    const _subscribe = () => {
-        Accelerometer.setUpdateInterval(1000);
-        setSubscription(Accelerometer.addListener(setData));
+    // Add a data in accelerometer interval using the setter
+    const addIntervalData = (intervalData) => {
+        return setAccelInter(prevData => ({
+            ...prevData,
+            interval: prevData.interval ? [...prevData.interval, intervalData] : []
+        }))
     };
 
-    const _unsubscribe = () => {
+    const subscribe = () => {
+        Accelerometer.setUpdateInterval(1000);
+        setSubscription(Accelerometer.addListener(addIntervalData));
+    };
+
+    const unsubscribe = () => {
         subscription && subscription.remove();
         setSubscription(null);
     };
 
+    // Subscribe and add start date in interval
+    const startInterval = () => {
+        subscribe();
+
+        // Init accelerometer interval with start date
+        const startDate = new Date();
+        setAccelInter({
+            startDate: startDate.toISOString(),
+            interval: []
+        });
+    }
+
+    // Unsubscribe to end interval
+    const endInterval = () => {
+        console.log(accelInter);
+        unsubscribe();
+    }
+
     useEffect(() => {
-        _subscribe();
-        if (timerStatus === TimerStatus.pause) {
-            console.log(`${x}, ${y}, ${z}`);
-        }
-        return () => _unsubscribe();
+        if (timerStatus === TimerStatus.play) startInterval(); // when play button pressed
+        else if (timerStatus === TimerStatus.pause) endInterval(); // when pause or stop button pressed
     }, [timerStatus]);
 };
 
