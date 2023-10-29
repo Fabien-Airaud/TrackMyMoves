72ff31d96ee61766da35addfa4af06e869c4e2ed
FabienTri <airaudfabien@gmail.com>
2023-10-29 11:54:59 -0400

    Remove sensor test components

--- components/navigation/AppTab.jsx
+++ components/navigation/AppTab.jsx
@@ -4,7 +4,6 @@ import { IconButton } from 'react-native-paper';
 import { Image } from 'react-native';
 import Move from '../move/Move';
 import Profile from '../profile/Profile';
-import SensorLocation from '../sensors/SensorLocation';
 
 const PlaceholderImageSource = require('../../assets/transparent-logo.png');
 
@@ -13,12 +12,6 @@ const Tab = createBottomTabNavigator();
 const MoveIcone = ({ focused, color, size }) => {
     return (
         <IconButton icon='map-marker-plus' focused={focused} iconColor={color} size={size} />
-    );
-};
-
-const Sensors = ({ focused, color, size }) => {
-    return (
-        <IconButton icon='motion-sensor' focused={focused} iconColor={color} size={size} />
     );
 };
 
@@ -32,7 +25,6 @@ const AppTab = () => {
     return (
         <Tab.Navigator screenOptions={{ headerTitle: 'TrackMyMoves', headerLeft: () => <Image source={PlaceholderImageSource} style={{ height: 50, width: 50, marginStart: 10 }} /> }}>
             <Tab.Screen name="Move" component={Move} options={{ tabBarIcon: MoveIcone }} />
-            <Tab.Screen name="Sensors" component={SensorLocation} options={{ tabBarIcon: Sensors }} />
             <Tab.Screen name="Profile" component={Profile} options={{ tabBarIcon: ProfileIcone }} />
         </Tab.Navigator>
     );
