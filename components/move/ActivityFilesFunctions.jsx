b0ae760da9a6b0d69c590ae066701a4a3135ea71
FabienTri <airaudfabien@gmail.com>
2023-10-29 15:25:53 -0400

    Store activity in local file system when save button pressed

--- components/move/ActivityFilesFunctions.jsx
+++ components/move/ActivityFilesFunctions.jsx
@@ -13,4 +13,12 @@ export const saveActivity = async (activity) => {
     // Check or add account directory
     const dirAccountUri = FileSystem.documentDirectory + activity.accountId;
     await ensureDirExists(dirAccountUri).catch(() => console.log(dirAccountUri + ' cannot be created'));
+
+    // Check or add activity type directory
+    const dirActivityTypeUri = dirAccountUri + '/' + activity.activityType;
+    await ensureDirExists(dirActivityTypeUri).catch(() => console.log(dirActivityTypeUri + ' cannot be created'));
+
+    // Add the activity in the activity type directory
+    const fileActivityUri = dirActivityTypeUri + '/' + activity.id;
+    await FileSystem.writeAsStringAsync(fileActivityUri, JSON.stringify(activity)).then(() => console.log(fileActivityUri + ' written'), () => console.log(fileActivityUri + ' cannot be writen'));
 }
