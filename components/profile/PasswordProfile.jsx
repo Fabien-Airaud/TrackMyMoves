167d11054513a45a5962778dd7938ad639ab37fb
FabienTri <airaudfabien@gmail.com>
2023-10-29 20:40:53 -0400

    Add confirmation for password, alerts if wrong and adapt style

--- components/profile/PasswordProfile.jsx
+++ components/profile/PasswordProfile.jsx
@@ -12,6 +12,8 @@ const PasswordProfile = ({ id, data }) => {
     const [edit, setEdit] = useState(false);
     const [value, setValue] = useState(data);
     const [secured, setSecured] = useState(true);
+    const [confirmValue, setConfirmValue] = useState(data);
+    const [confirmSecured, setConfirmSecured] = useState(true);
 
     // Style variables
     const { colors, fontSizes } = useTheme();
@@ -22,12 +24,15 @@ const PasswordProfile = ({ id, data }) => {
             alignItems: 'center',
             width: '100%',
         },
+        verticalView: {
+            width: '75%'
+        },
         text: {
             color: colors.text,
             fontSize: fontSizes.sm
         },
         input: {
-            width: '75%',
+            width: '100%',
             backgroundColor: colors.inputFill,
             marginVertical: '2%',
             paddingHorizontal: 10
@@ -39,42 +44,63 @@ const PasswordProfile = ({ id, data }) => {
 
     const dispatchPassword = () => {
         if (data !== value) {
-            dispatch(
-                updatePassword({
-                    id: id,
-                    password: value
-                })
-            );
-            dispatch(
-                updateLogPassword({
-                    id: id,
-                    password: value
-                })
-            );
-            console.log(data + ' -> ' + value);
+            if (value === confirmValue) {
+                dispatch(
+                    updatePassword({
+                        id: id,
+                        password: value
+                    })
+                );
+                dispatch(
+                    updateLogPassword({
+                        id: id,
+                        password: value
+                    })
+                );
+                console.log(data + ' -> ' + value);
+            } else {
+                alert('Password and confirmation password are different');
+                return ;
+            }
+        } else if (value !== confirmValue) {
+            alert('Password and confirmation password are different');
+            setConfirmValue(data);
         }
         setEdit(false);
     };
 
     return (
         <View style={styles.view}>
-            <TextInput
-                label={edit ? 'Password' : ''}
-                value={value}
-                secureTextEntry={secured}
-                disabled={!edit}
-                onChangeText={text => setValue(text)}
-                right={<TextInput.Icon icon={secured ? 'eye' : 'eye-off'} onPress={() => setSecured(!secured)} color={colors.placeholder} />}
-                inputMode={'text'}
-                textColor={colors.text}
-                theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
-                style={styles.input}
-            />
+            <View style={styles.verticalView}>
+                <TextInput
+                    label={edit ? 'Password' : ''}
+                    value={value}
+                    secureTextEntry={secured}
+                    disabled={!edit}
+                    onChangeText={text => setValue(text)}
+                    right={<TextInput.Icon icon={secured ? 'eye' : 'eye-off'} onPress={() => setSecured(!secured)} color={colors.placeholder} />}
+                    inputMode={'text'}
+                    textColor={colors.text}
+                    theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
+                    style={styles.input}
+                />
+                <TextInput
+                    label='Confirm password'
+                    value={confirmValue}
+                    secureTextEntry={confirmSecured}
+                    onChangeText={text => setConfirmValue(text)}
+                    right={<TextInput.Icon icon={confirmSecured ? 'eye' : 'eye-off'} onPress={() => setConfirmSecured(!confirmSecured)} color={colors.placeholder} />}
+                    inputMode={'text'}
+                    textColor={colors.text}
+                    theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
+                    style={edit ? styles.input : {display: 'none'}}
+                />
+            </View>
             <View style={[styles.view, { paddingHorizontal: 5, width: 3 * fontSizes.sm }]}>
                 {edit ?
                     <>
                         <IconButton icon='check-bold' onPress={() => dispatchPassword()} iconColor={colors.text} containerColor={colors.success} size={fontSizes.sm} />
-                        <IconButton icon='close-thick' onPress={() => { setEdit(false); setValue(data); }} iconColor={colors.text} containerColor={colors.error} size={fontSizes.sm} />
+                        <IconButton icon='close-thick' onPress={() => { setEdit(false); setValue(data); setConfirmValue(data) }} iconColor={colors.text} containerColor={colors.error} size={fontSizes.sm} />
                     </>
                     : <IconButton icon='pencil' onPress={() => setEdit(true)} iconColor={colors.text} containerColor={colors.primary} size={fontSizes.sm} />
                 }
