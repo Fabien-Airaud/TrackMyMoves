f659a65358a3ecf278620d5b22e6c1b0064eca3c
FabienTri <airaudfabien@gmail.com>
2023-10-31 10:52:50 -0400

    Add mock list to test styling

--- components/history/History.jsx
+++ components/history/History.jsx
@@ -1,5 +1,8 @@
 import { useTheme } from '@react-navigation/native';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ListItem } from '@rneui/themed';
+import { IconButton } from 'react-native-paper';
+import { ScrollView, StyleSheet } from 'react-native';
+import { ListItemChevron } from '@rneui/base/dist/ListItem/ListItem.Chevron';
 
 const History = () => {
     // Style variables
@@ -10,12 +13,32 @@ const History = () => {
             width: '100%',
             padding: '5%',
             backgroundColor: colors.background
+        },
+        listItem: {
+            padding: 5,
+            backgroundColor: colors.inputFill
+        },
+        text: {
+            color: colors.text
         }
     });
 
     return (
         <ScrollView style={styles.page}>
-            <Text> History </Text>
+            <ListItem containerStyle={styles.listItem} bottomDivider={true}>
+                <IconButton icon='clock' iconColor={colors.text} />
+                <ListItem.Content>
+                    <ListItem.Title style={styles.text}>Activity type 1</ListItem.Title>
+                </ListItem.Content>
+                <ListItemChevron color={colors.text} />
+            </ListItem>
+            <ListItem containerStyle={styles.listItem}>
+                <IconButton icon='clock' iconColor={colors.text} />
+                <ListItem.Content>
+                    <ListItem.Title style={styles.text}>Activity type 2</ListItem.Title>
+                </ListItem.Content>
+                <ListItemChevron color={colors.text} />
+            </ListItem>
         </ScrollView>
     );
 };
