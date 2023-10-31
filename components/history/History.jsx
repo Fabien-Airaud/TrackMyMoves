6b5bebf911eeb4e25afd240b69b8699e42eefd87
FabienTri <airaudfabien@gmail.com>
2023-10-31 15:23:15 -0400

    Display list with directory titles (activity types)

--- components/history/History.jsx
+++ components/history/History.jsx
@@ -1,15 +1,18 @@
 import { useTheme } from '@react-navigation/native';
 import { ListItem } from '@rneui/themed';
 import { useEffect, useState } from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 import { IconButton } from 'react-native-paper';
 import { useSelector } from 'react-redux';
 
 import { getActivityTypes } from './HistoryFunctions';
+import { ActivityTypes } from '../move/TopMove';
 
 const History = ({ navigation }) => {
     // State variables
+    const [unsubscribeFocus, setUnsubscribeFocus] = useState(() => { });
     const [expanded, setExpanded] = useState(false);
+    const [list, setList] = useState([]);
 
     // Style variables
     const { colors, fontsizes } = useTheme();
@@ -36,54 +39,51 @@ const History = ({ navigation }) => {
     const logAcc = useSelector((state) => state.logIn.account);
 
     useEffect(() => {
-        const unsubscribe = navigation.addListener('focus', async () => {
-            console.log(await getActivityTypes(logAcc.id));
-        });
-        
-        return unsubscribe;
-    }, [navigation]);
-
-    useEffect(() => {
         (async () => {
-            console.log(await getActivityTypes(logAcc.id));
-        })();
-    }, []);
+            const unsubscribe = await navigation.addListener('focus', async () => {
+                setList(await getActivityTypes(logAcc.id));
+            });
+            setUnsubscribeFocus(unsubscribe);
+        })(setUnsubscribeFocus, setList);
+
+        return unsubscribeFocus;
+    }, [navigation]);
 
     return (
         <ScrollView style={styles.page}>
-            <ListItem.Accordion
-                content={
-                    <>
-                        <IconButton icon='clock' iconColor={colors.text} />
-                        <ListItem.Content>
-                            <ListItem.Title style={styles.text}>List Accordion</ListItem.Title>
-                        </ListItem.Content>
-                    </>
-                }
-                isExpanded={expanded}
-                onPress={() => {
-                    setExpanded(!expanded);
-                }}
-                icon={
-                    <IconButton icon='chevron-down' iconColor={colors.text} />
-                }
-                bottomDivider={true}
-                containerStyle={styles.accordion}
-            >
-                <ListItem containerStyle={styles.listItem} bottomDivider={true}>
-                    <ListItem.Content>
-                        <ListItem.Title style={styles.text}>Activity type 1</ListItem.Title>
-                    </ListItem.Content>
-                    <ListItem.Chevron color={colors.text} />
-                </ListItem>
-                <ListItem containerStyle={styles.listItem}>
-                    <ListItem.Content>
-                        <ListItem.Title style={styles.text}>Activity type 2</ListItem.Title>
-                    </ListItem.Content>
-                    <ListItem.Chevron color={colors.text} />
-                </ListItem>
-
-            </ListItem.Accordion>
+            {
+                list.map((value, index) => {
+                    return (
+                        <ListItem.Accordion
+                            key={index}
+                            content={<>
+                                <IconButton icon='clock' iconColor={colors.text} />
+                                <ListItem.Content>
+                                    <ListItem.Title style={styles.text}> {value} </ListItem.Title>
+                                </ListItem.Content>
+                            </>}
+                            isExpanded={expanded}
+                            // onPress={() => {setExpanded(!expanded)}}
+                            icon={<IconButton icon='chevron-down' iconColor={colors.text} />}
+                            bottomDivider={true}
+                            containerStyle={styles.accordion}
+                        >
+                            <ListItem containerStyle={styles.listItem} bottomDivider={true}>
+                                <ListItem.Content>
+                                    <ListItem.Title style={styles.text}>Activity 1</ListItem.Title>
+                                </ListItem.Content>
+                                <ListItem.Chevron color={colors.text} />
+                            </ListItem>
+                            <ListItem containerStyle={styles.listItem}>
+                                <ListItem.Content>
+                                    <ListItem.Title style={styles.text}>Activity 2</ListItem.Title>
+                                </ListItem.Content>
+                                <ListItem.Chevron color={colors.text} />
+                            </ListItem>
+                        </ListItem.Accordion>
+                    )
+                })
+            }
         </ScrollView>
     );
 };
