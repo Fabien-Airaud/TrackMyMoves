0b5bed77e2db92c98fdb1318dcdd44a9f494ca6d
FabienTri <airaudfabien@gmail.com>
2023-10-31 10:20:11 -0400

    Add history page

--- components/navigation/AppTab.jsx
+++ components/navigation/AppTab.jsx
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { IconButton } from 'react-native-paper';
 
 import { Image } from 'react-native';
+import History from '../history/History';
 import Move from '../move/Move';
 import Profile from '../profile/Profile';
 
@@ -9,13 +10,19 @@ const PlaceholderImageSource = require('../../assets/transparent-logo.png');
 
 const Tab = createBottomTabNavigator();
 
-const MoveIcone = ({ focused, color, size }) => {
+const MoveIcon = ({ focused, color, size }) => {
     return (
         <IconButton icon='map-marker-plus' focused={focused} iconColor={color} size={size} />
     );
 };
 
-const ProfileIcone = ({ focused, color, size }) => {
+const HistoryIcon = ({ focused, color, size }) => {
+    return (
+        <IconButton icon='history' focused={focused} iconColor={color} size={size} />
+    );
+};
+
+const ProfileIcon = ({ focused, color, size }) => {
     return (
         <IconButton icon='account-edit' focused={focused} iconColor={color} size={size} />
     );
@@ -24,8 +31,9 @@ const ProfileIcone = ({ focused, color, size }) => {
 const AppTab = () => {
     return (
         <Tab.Navigator screenOptions={{ headerTitle: 'TrackMyMoves', headerLeft: () => <Image source={PlaceholderImageSource} style={{ height: 50, width: 50, marginStart: 10 }} /> }}>
-            <Tab.Screen name="Move" component={Move} options={{ tabBarIcon: MoveIcone }} />
-            <Tab.Screen name="Profile" component={Profile} options={{ tabBarIcon: ProfileIcone }} />
+            <Tab.Screen name="Move" component={Move} options={{ tabBarIcon: MoveIcon }} />
+            <Tab.Screen name="History" component={History} options={{ tabBarIcon: HistoryIcon }} />
+            <Tab.Screen name="Profile" component={Profile} options={{ tabBarIcon: ProfileIcon }} />
         </Tab.Navigator>
     );
 };
