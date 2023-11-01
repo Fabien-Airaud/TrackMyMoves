f05379e7d6f0b53a2e8b5ddba5fd69a5b62bb143
FabienTri <airaudfabien@gmail.com>
2023-11-01 19:21:59 -0400

    history function return a list of activity types directly

--- components/history/ActivityList.jsx
+++ components/history/ActivityList.jsx
@@ -1,15 +1,9 @@
 import { useTheme } from '@react-navigation/native';
 import { ListItem } from '@rneui/themed';
-import { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { IconButton } from 'react-native-paper';
 
-import { ActivityTypes } from "../move/TopMove";
-
 const ActivityList = ({ list }) => {
-    // State variables
-    const [expanded, setExpanded] = useState(false);
-
     // Style variables
     const { colors } = useTheme();
     const styles = StyleSheet.create({
@@ -26,20 +20,20 @@ const ActivityList = ({ list }) => {
     });
 
     return list.map((value, index) => {
-        const activityType = ActivityTypes.find(type => type.value === value);
-        console.log(activityType);
 
         return (
             <ListItem.Accordion
                 key={index}
                 content={<>
-                    <IconButton icon={activityType.leadingIcon} iconColor={colors.text} />
+                    <IconButton icon={value.leadingIcon} iconColor={colors.text} />
                     <ListItem.Content>
-                        <ListItem.Title style={styles.text}> {activityType.label} </ListItem.Title>
+                        <ListItem.Title style={styles.text}> {value.label} </ListItem.Title>
                     </ListItem.Content>
                 </>}
-                isExpanded={expanded}
-                // onPress={() => {setExpanded(!expanded)}}
+                // isExpanded={listExpanded[index]}
+                // isExpanded={expanded}
+                // onPress={() => setListExpanded(changeExpandedList(listExpanded, index))}
+                // onPress={() => setExpanded(!expanded)}
                 icon={<IconButton icon='chevron-down' iconColor={colors.text} />}
                 bottomDivider={true}
                 containerStyle={styles.accordion}
--- components/history/HistoryFunctions.jsx
+++ components/history/HistoryFunctions.jsx
@@ -1,5 +1,13 @@
 import * as FileSystem from 'expo-file-system';
 
+import { ActivityTypes } from "../move/TopMove";
+
+const createActivityTypesForList = (activityTypeValue) => {
+    const activityType = ActivityTypes.find(type => type.value === activityTypeValue);
+
+    return activityType;
+};
+
 export const getActivityTypes = async (userId) => {
     // Get user directory info
     const userDirUri = FileSystem.documentDirectory + userId;
@@ -16,7 +24,7 @@ export const getActivityTypes = async (userId) => {
     const activityTypes = [];
     for (let element of elements) {
         const fileInfo = await FileSystem.getInfoAsync(userDirUri + '/' + element);
-        if (fileInfo.isDirectory) activityTypes.push(element);
+        if (fileInfo.isDirectory) activityTypes.push(createActivityTypesForList(element));
     }
     return activityTypes; // Return all directories in user directory
 
