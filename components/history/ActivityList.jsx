ff3e3845f78a6e23a444d2fc6b8d0baad11ea23a
FabienTri <airaudfabien@gmail.com>
2023-11-01 19:30:55 -0400

    Use Accordion instead of List.Accordion

--- components/history/ActivityList.jsx
+++ components/history/ActivityList.jsx
@@ -3,14 +3,12 @@ import { ListItem } from '@rneui/themed';
 import { StyleSheet } from 'react-native';
 import { IconButton } from 'react-native-paper';
 
+import Accordion from './Accordion';
+
 const ActivityList = ({ list }) => {
     // Style variables
     const { colors } = useTheme();
     const styles = StyleSheet.create({
-        accordion: {
-            borderBottomWidth: 2,
-            backgroundColor: colors.inputFill
-        },
         listItem: {
             backgroundColor: colors.inputFill
         },
@@ -22,7 +20,7 @@ const ActivityList = ({ list }) => {
     return list.map((value, index) => {
 
         return (
-            <ListItem.Accordion
+            <Accordion
                 key={index}
                 content={<>
                     <IconButton icon={value.leadingIcon} iconColor={colors.text} />
@@ -30,28 +28,24 @@ const ActivityList = ({ list }) => {
                         <ListItem.Title style={styles.text}> {value.label} </ListItem.Title>
                     </ListItem.Content>
                 </>}
-                // isExpanded={listExpanded[index]}
-                // isExpanded={expanded}
-                // onPress={() => setListExpanded(changeExpandedList(listExpanded, index))}
-                // onPress={() => setExpanded(!expanded)}
                 icon={<IconButton icon='chevron-down' iconColor={colors.text} />}
                 bottomDivider={true}
-                containerStyle={styles.accordion}
-            >
-                <ListItem containerStyle={styles.listItem} bottomDivider={true}>
-                    <ListItem.Content>
-                        <ListItem.Title style={styles.text}>Activity 1</ListItem.Title>
-                    </ListItem.Content>
-                    <ListItem.Chevron color={colors.text} />
-                </ListItem>
-                <ListItem containerStyle={styles.listItem}>
-                    <ListItem.Content>
-                        <ListItem.Title style={styles.text}>Activity 2</ListItem.Title>
-                    </ListItem.Content>
-                    <ListItem.Chevron color={colors.text} />
-                </ListItem>
-            </ListItem.Accordion>
-        )
+                children={<>
+                    <ListItem containerStyle={styles.listItem} bottomDivider={true}>
+                        <ListItem.Content>
+                            <ListItem.Title style={styles.text}>Activity 1</ListItem.Title>
+                        </ListItem.Content>
+                        <ListItem.Chevron color={colors.text} />
+                    </ListItem>
+                    <ListItem containerStyle={styles.listItem}>
+                        <ListItem.Content>
+                            <ListItem.Title style={styles.text}>Activity 2</ListItem.Title>
+                        </ListItem.Content>
+                        <ListItem.Chevron color={colors.text} />
+                    </ListItem>
+                </>}
+            />
+        );
     })
 };
 
