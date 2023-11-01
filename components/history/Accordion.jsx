bb234c7826659e7a59675c58f111b915cbedc7fe
FabienTri <airaudfabien@gmail.com>
2023-11-01 19:29:58 -0400

    Create Accordion component so it can expand itself

---
+++ components/history/Accordion.jsx
@@ -0,0 +1,33 @@
+import { useTheme } from '@react-navigation/native';
+import { ListItem } from '@rneui/themed';
+import { useState } from 'react';
+import { StyleSheet } from 'react-native';
+
+const Accordion = ({ content, icon, bottomDivider, children }) => {
+    // State variable
+    const [expanded, setExpanded] = useState(false);
+
+    // Style variables
+    const { colors } = useTheme();
+    const styles = StyleSheet.create({
+        accordion: {
+            borderBottomWidth: 2,
+            backgroundColor: colors.inputFill
+        }
+    });
+
+    return (
+        <ListItem.Accordion
+            content={content}
+            isExpanded={expanded}
+            onPress={() => setExpanded(!expanded)}
+            icon={icon}
+            bottomDivider={bottomDivider}
+            containerStyle={styles.accordion}
+        >
+            {children}
+        </ListItem.Accordion>
+    );
+};
+
+export default Accordion;
