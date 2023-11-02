536c9a14446709d3b343c0ad561c46c3912893eb
FabienTri <airaudfabien@gmail.com>
2023-11-01 23:03:25 -0400

    Get all activities in directory and add it

--- components/history/HistoryFunctions.jsx
+++ components/history/HistoryFunctions.jsx
@@ -2,10 +2,27 @@ import * as FileSystem from 'expo-file-system';
 
 import { ActivityTypes } from "../move/TopMove";
 
-const createActivityTypesForList = (activityTypeValue) => {
+const getActivities = async (activityTypeDirUri) => {
+    const elements = await FileSystem.readDirectoryAsync(activityTypeDirUri); // return elements in activity type directory
+
+    // Get JSON from files
+    const activities = [];
+    for (let element of elements) {
+        const fileUri = activityTypeDirUri + '/' + element;
+        const fileInfo = await FileSystem.getInfoAsync(fileUri);
+
+        if (fileInfo.exists && !fileInfo.isDirectory) { // if is a file
+            const fileinJSON = await FileSystem.readAsStringAsync(fileUri); // get file's content in string
+            activities.push(JSON.parse(fileinJSON)); // push file's content in its object shape
+        }
+    }
+    return activities; // Return all activities in activity type directory
+}
+
+const createActivityList = (activityTypeValue, activities) => {
     const activityType = ActivityTypes.find(type => type.value === activityTypeValue);
 
-    return activityType;
+    return {...activityType, activities: activities};
 };
 
 export const getActivityTypes = async (userId) => {
@@ -23,9 +40,13 @@ export const getActivityTypes = async (userId) => {
     // Filter elements, keep directories
     const activityTypes = [];
     for (let element of elements) {
-        const fileInfo = await FileSystem.getInfoAsync(userDirUri + '/' + element);
-        if (fileInfo.isDirectory) activityTypes.push(createActivityTypesForList(element));
+        const dirUri = userDirUri + '/' + element;
+        const fileInfo = await FileSystem.getInfoAsync(dirUri);
+
+        if (fileInfo.isDirectory) {
+            const activities = await getActivities(dirUri); // Get all activities in activity type directory
+            activityTypes.push(createActivityList(element, activities));
+        }
     }
     return activityTypes; // Return all directories in user directory
-
 }
