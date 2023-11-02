99dd6cbe9655fd52faeea5cf412fe8956a3c2a61
FabienTri <airaudfabien@gmail.com>
2023-11-02 17:32:40 -0400

    Remove default logged in account

--- redux/logInSlice.jsx
+++ redux/logInSlice.jsx
@@ -3,18 +3,8 @@ import { createSlice } from '@reduxjs/toolkit';
 export const logInSlice = createSlice({
     name: 'logIn',
     initialState: {
-        account: {
-            id: '4fbbfd11-4e35-482f-b69d-8fbb3d4f175d',
-            emailAddress: 'airaudfabien@gmail.com',
-            password: 'Fabien2002',
-            firstName: 'Fabien',
-            lastName: 'Airaud',
-            birthdate: '2002-11-26T05:00:00.000Z',
-            height: '192',
-            weight: '63',
-            country: 'Canada'
-        },
-        rememberMe: true
+        account: undefined,
+        rememberMe: false
     },
     reducers: {
         logInAccount: (state, action) => {
