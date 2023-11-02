1485f98cbf67ddf8e3b94070fc969872ce474ad9
FabienTri <airaudfabien@gmail.com>
2023-11-01 23:52:37 -0400

    Display activity in the listItem

--- components/history/ActivityList.jsx
+++ components/history/ActivityList.jsx
@@ -4,6 +4,7 @@ import { StyleSheet } from 'react-native';
 import { IconButton } from 'react-native-paper';
 
 import Accordion from './Accordion';
+import { formatTime } from '../move/FormatTime';
 
 const ActivityList = ({ list }) => {
     // Style variables
@@ -12,8 +13,11 @@ const ActivityList = ({ list }) => {
         listItem: {
             backgroundColor: colors.inputFill
         },
-        text: {
+        title: {
             color: colors.text
+        },
+        subtitle: {
+            color: colors.placeholder
         }
     });
 
@@ -25,25 +29,30 @@ const ActivityList = ({ list }) => {
                 content={<>
                     <IconButton icon={value.leadingIcon} iconColor={colors.text} />
                     <ListItem.Content>
-                        <ListItem.Title style={styles.text}> {value.label} </ListItem.Title>
+                        <ListItem.Title style={styles.title}> {value.label} </ListItem.Title>
                     </ListItem.Content>
                 </>}
                 icon={<IconButton icon='chevron-down' iconColor={colors.text} />}
                 bottomDivider={true}
-                children={<>
-                    <ListItem containerStyle={styles.listItem} bottomDivider={true}>
-                        <ListItem.Content>
-                            <ListItem.Title style={styles.text}>Activity 1</ListItem.Title>
-                        </ListItem.Content>
-                        <ListItem.Chevron color={colors.text} />
-                    </ListItem>
-                    <ListItem containerStyle={styles.listItem}>
-                        <ListItem.Content>
-                            <ListItem.Title style={styles.text}>Activity 2</ListItem.Title>
-                        </ListItem.Content>
-                        <ListItem.Chevron color={colors.text} />
-                    </ListItem>
-                </>}
+                children={value.activities.map((activity, num) => {
+                    // Get elapsed time (in the last interval) in string formatted
+                    const lastInterval = activity.intervals[activity.intervals.length - 1];
+                    const time = formatTime(lastInterval.endTime * 1000); // (* 1000) -> time from sec to msec
+
+                    // Get start date in string
+                    const startDate = new Date(activity.startDate);
+                    const dateString = startDate.toLocaleString();
+
+                    return (
+                        <ListItem key={num} containerStyle={styles.listItem} bottomDivider={true}>
+                            <ListItem.Content>
+                                <ListItem.Title style={styles.title}> {`${value.label} - ${time}`} </ListItem.Title>
+                                <ListItem.Subtitle style={styles.subtitle}> {dateString} </ListItem.Subtitle>
+                            </ListItem.Content>
+                            <ListItem.Chevron color={colors.text} />
+                        </ListItem>
+                    );
+                })}
             />
         );
     })
