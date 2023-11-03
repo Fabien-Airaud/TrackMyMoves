c95a645107c4600b19b4a2d0e308a5fa2730d800
FabienTri <airaudfabien@gmail.com>
2023-11-02 20:59:32 -0400

    Add param to formatTime (space or not)

--- components/move/FormatTime.jsx
+++ components/move/FormatTime.jsx
@@ -1,9 +1,10 @@
 /**
  * Format the time received in a string to display
  * @param {number} time - The time to format, in milliseconds
+ * @param {boolean} space - If it add spaces around seperators
  * @returns {string} a string representing the time in hours, minutes and seconds
  */
-export const formatTime = (time) => {
+export const formatTime = (time, space = true) => {
     // Time in seperated variables
     const hours = Math.floor(time / 1000 / 60 / 60);
     const mins = Math.floor((time / 1000 / 60) % 60);
@@ -13,6 +14,7 @@ export const formatTime = (time) => {
     const stringHours = hours < 10 ? `0${hours}` : `${hours}`
     const stringMins = mins < 10 ? `0${mins}` : `${mins}`
     const stringSecs = seconds < 10 ? `0${seconds}` : `${seconds}`
+    const seperator = space ? ' : ' : ':';
 
-    return `${stringHours} : ${stringMins} : ${stringSecs}`;
+    return stringHours + seperator + stringMins + seperator + stringSecs;
 }
