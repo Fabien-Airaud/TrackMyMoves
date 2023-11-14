d7141694f51f3ca5c8b43c2062e5410870f8c090
FabienTri <airaudfabien@gmail.com>
2023-11-14 14:04:06 -0500

    Use base template in app view

--- backend/track_my_moves/views.py
+++ backend/track_my_moves/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<html><body><p> Page d'accueil ! </p></body></html>")
+    return render(request, "base.html", {"title": "Home"})
 
 def contact(request):
-    return HttpResponse("<html><body><p> Page de contact ! </p></body></html>")
+    return render(request, "base.html", {"title": "Contact"})
