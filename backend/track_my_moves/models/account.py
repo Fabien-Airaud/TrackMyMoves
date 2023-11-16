1c992dd7b8370cdaa7c008360c8a081bc4a9cf43
FabienTri <airaudfabien@gmail.com>
2023-11-16 18:52:04 -0500

    Link user to account

--- backend/track_my_moves/models/account.py
+++ backend/track_my_moves/models/account.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Account(models.Model):
-    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField("User", on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     birthdate = models.DateField(auto_now=False, auto_now_add=False)
