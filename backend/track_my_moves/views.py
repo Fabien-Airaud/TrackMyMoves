28e47d9f632fbb63579427959df84cdbae21314c
FabienTri <airaudfabien@gmail.com>
2023-11-17 15:56:03 -0500

    Check user and if have permissions -> redirect

--- backend/track_my_moves/views.py
+++ backend/track_my_moves/views.py
@@ -1,8 +1,19 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 APP_DIR_PATH = "track_my_moves/"
 
 def logIn(request):
+    if request.method == "POST":
+        logInEmail = request.POST["logInEmail"]
+        logInPassword = request.POST["logInPassword"]
+        
+        user = authenticate(request, username=logInEmail, password=logInPassword)
+        if user is not None and user.is_superuser and user.is_active:
+            login(request, user)
+            return HttpResponseRedirect("usersAdmin")
+    
     return render(request, APP_DIR_PATH + "logIn.html")
 
 def usersAdmin(request):
