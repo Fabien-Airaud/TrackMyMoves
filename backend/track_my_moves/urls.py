bbb9f5423fd5a7c214861a6f9f25100a8a91d473
FabienTri <airaudfabien@gmail.com>
2023-11-17 18:44:03 -0500

    Add home url, just like default

--- backend/track_my_moves/urls.py
+++ backend/track_my_moves/urls.py
@@ -3,6 +3,7 @@ from .views import home, logIn, logOut, usersAdmin
 
 urlpatterns = [
     path('', home, name='home'),
+    path('home', home, name='home'),
     path('logIn', logIn, name='logIn'),
     path('logOut', logOut, name='logOut'),
     path('usersAdmin', usersAdmin, name='usersAdmin')
