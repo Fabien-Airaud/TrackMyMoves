7fe414239ce85547a5ad428ef00fb1d149010682
FabienTri <airaudfabien@gmail.com>
2023-11-17 18:43:38 -0500

    Add logOut url

--- backend/track_my_moves/urls.py
+++ backend/track_my_moves/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, logIn, usersAdmin
+from .views import home, logIn, logOut, usersAdmin
 
 urlpatterns = [
     path('', home, name='home'),
     path('logIn', logIn, name='logIn'),
+    path('logOut', logOut, name='logOut'),
     path('usersAdmin', usersAdmin, name='usersAdmin')
 ]
