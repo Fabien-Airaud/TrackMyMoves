ed45c9eac04d819976a527a893aaac2c96d32bca
FabienTri <airaudfabien@gmail.com>
2023-11-17 15:09:33 -0500

    Add view for users admin

--- backend/track_my_moves/views.py
+++ backend/track_my_moves/views.py
@@ -5,5 +5,5 @@ APP_DIR_PATH = "track_my_moves/"
 def logIn(request):
     return render(request, APP_DIR_PATH + "logIn.html")
 
-# def contact(request):
-#     return render(request, APP_DIR_PATH + "contact.html")
+def usersAdmin(request):
+    return render(request, APP_DIR_PATH + "usersAdmin.html")
