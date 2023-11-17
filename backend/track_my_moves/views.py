f0d4551c6ffa7f131dbf22ef0d7c94220d1533e5
FabienTri <airaudfabien@gmail.com>
2023-11-17 16:13:57 -0500

    Create home view (redirect to correct view, if logged in)

--- backend/track_my_moves/views.py
+++ backend/track_my_moves/views.py
@@ -4,6 +4,14 @@ from django.contrib.auth import authenticate, login
 
 APP_DIR_PATH = "track_my_moves/"
 
+def home(request):
+    user = request.user
+    if user.is_authenticated and user.is_superuser and user.is_active:
+        # utilisateur connecté
+        return HttpResponseRedirect("usersAdmin")
+    else:
+        return HttpResponseRedirect("logIn")
+
 def logIn(request):
     if request.method == "POST":
         logInEmail = request.POST["logInEmail"]
