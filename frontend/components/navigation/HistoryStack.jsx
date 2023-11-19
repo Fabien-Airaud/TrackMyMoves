96adeed2ebcdba5df9366d6b822e14eba5e2723e
FabienTri <airaudfabien@gmail.com>
2023-11-19 10:07:28 -0500

    Create HistoryStack to use Informations page

---
+++ frontend/components/navigation/HistoryStack.jsx
@@ -0,0 +1,17 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import History from '../history/History';
+import Informations from '../history/Informations'
+
+const Stack = createNativeStackNavigator();
+
+const HistoryStack = () => {
+  return (
+    <Stack.Navigator initialRouteName="History">
+      <Stack.Screen name='History' component={History} options={{ headerShown: false }} />
+      <Stack.Screen name='Informations' component={Informations} options={{ headerShown: true, title: "Activity informations" }} />
+    </Stack.Navigator>
+  );
+};
+
+export default HistoryStack;
