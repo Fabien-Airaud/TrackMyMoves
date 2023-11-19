08af558e411b3fb1a2fcdcf16a74521b8cca4d93
FabienTri <airaudfabien@gmail.com>
2023-11-19 11:49:07 -0500

    Create ActivityType file

---
+++ frontend/components/move/ActivityTypes.jsx
@@ -0,0 +1,22 @@
+export default ActivityTypes = [
+    {
+        label: 'Walking',
+        value: 'walking',
+        leadingIcon: 'walk'
+    },
+    {
+        label: 'Running',
+        value: 'running',
+        leadingIcon: 'run'
+    },
+    {
+        label: 'Cycling',
+        value: 'cycling',
+        leadingIcon: 'bike'
+    },
+    {
+        label: 'Cardio',
+        value: 'cardio',
+        leadingIcon: 'timer'
+    }
+];
