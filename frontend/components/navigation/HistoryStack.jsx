48cdd365cafdfc821b865215eecef60443b89a14
FabienTri <airaudfabien@gmail.com>
2023-11-19 11:50:00 -0500

    Change header title according to the activity type

--- frontend/components/navigation/HistoryStack.jsx
+++ frontend/components/navigation/HistoryStack.jsx
@@ -2,14 +2,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import History from '../history/History';
 import Informations from '../history/Informations'
+import ActivityTypes from '../move/ActivityTypes';
 
 const Stack = createNativeStackNavigator();
 
 const HistoryStack = () => {
+  const getHeaderTitle = (activity) => {
+    const activityType = ActivityTypes.find((activityType) => activityType.value == activity.activityType);
+    
+    return activityType ? activityType.label : "Activity informations"
+  }
+
   return (
     <Stack.Navigator initialRouteName="History">
       <Stack.Screen name='History' component={History} options={{ headerShown: false }} />
-      <Stack.Screen name='Informations' component={Informations} options={{ headerShown: true, title: "Activity informations" }} />
+      <Stack.Screen name='Informations' component={Informations} options={({route}) => ({ headerShown: true, title: getHeaderTitle(route.params.activity) })} />
     </Stack.Navigator>
   );
 };
