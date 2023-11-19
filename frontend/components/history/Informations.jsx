b424be159170360dd28457554f7e41b86f5e5231
FabienTri <airaudfabien@gmail.com>
2023-11-19 10:07:03 -0500

    Create informations page

---
+++ frontend/components/history/Informations.jsx
@@ -0,0 +1,29 @@
+import { useTheme } from '@react-navigation/native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
+
+const History = ({ navigation }) => {
+    // Style variables
+    const { colors, fontSizes } = useTheme();
+    const styles = StyleSheet.create({
+        page: {
+            minHeight: '100%',
+            width: '100%',
+            padding: '5%',
+            backgroundColor: colors.background
+        },
+        text: {
+            textAlign: 'center',
+            margin: 10,
+            color: colors.text,
+            fontSize: fontSizes.md
+        }
+    });
+
+    return (
+        <ScrollView style={styles.page}>
+            <Text>Activity informations</Text>
+        </ScrollView>
+    );
+};
+
+export default History;
