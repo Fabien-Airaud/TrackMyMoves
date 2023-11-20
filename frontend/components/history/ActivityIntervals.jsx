1e573e3e2cb7221e0ca6757cd6a2488e6dfbe007
FabienTri <airaudfabien@gmail.com>
2023-11-20 15:52:50 -0500

    Add a mock datatable

--- frontend/components/history/ActivityIntervals.jsx
+++ frontend/components/history/ActivityIntervals.jsx
@@ -1,13 +1,51 @@
 import { useTheme } from '@react-navigation/native';
 import { ScrollView, StyleSheet, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { DataTable } from 'react-native-paper';
 
 const ActivityIntervals = ({ navigation, route: { params: { activity } } }) => {
+    // State variables
+    const [page, setPage] = useState(0);
+    const [numberOfItemsPerPageList] = useState([5, 10, 15]);
+    const [itemsPerPage, onItemsPerPageChange] = useState(numberOfItemsPerPageList[0]);
+
+    const [items] = useState([
+        {
+          key: 1,
+          name: 'Cupcake',
+          calories: 356,
+          fat: 16,
+        },
+        {
+          key: 2,
+          name: 'Eclair',
+          calories: 262,
+          fat: 16,
+        },
+        {
+          key: 3,
+          name: 'Frozen yogurt',
+          calories: 159,
+          fat: 6,
+        },
+        {
+          key: 4,
+          name: 'Gingerbread',
+          calories: 305,
+          fat: 3.7,
+        },
+       ]);
+
+    const from = page * itemsPerPage;
+    const to = Math.min((page + 1) * itemsPerPage, items.length);
+
     // Style variables
     const { colors, fontSizes } = useTheme();
     const styles = StyleSheet.create({
         page: {
-            minHeight: '100%',
-            width: '100%',
+            flexDirection: 'row',
+            height: '100%',
+            minWidth: '100%',
             padding: '5%',
             backgroundColor: colors.background
         },
@@ -18,11 +56,39 @@ const ActivityIntervals = ({ navigation, route: { params: { activity } } }) => {
             fontSize: fontSizes.sm
         }
     });
+  
+    useEffect(() => {
+      setPage(0);
+    }, [itemsPerPage]);
 
     return (
-        <ScrollView style={styles.page}>
-            <Text style={styles.text}>{JSON.stringify(activity)}</Text>
-        </ScrollView>
+        <DataTable>
+            <DataTable.Header>
+                <DataTable.Title>Dessert</DataTable.Title>
+                <DataTable.Title numeric>Calories</DataTable.Title>
+                <DataTable.Title numeric>Fat</DataTable.Title>
+            </DataTable.Header>
+
+            {items.slice(from, to).map((item, index) => (
+                <DataTable.Row key={index}>
+                <DataTable.Cell>{item.name}</DataTable.Cell>
+                <DataTable.Cell numeric>{item.calories}</DataTable.Cell>
+                <DataTable.Cell numeric>{item.fat}</DataTable.Cell>
+                </DataTable.Row>
+            ))}
+
+            <DataTable.Pagination
+                page={page}
+                numberOfPages={Math.ceil(items.length / itemsPerPage)}
+                onPageChange={(page) => setPage(page)}
+                label={`${from + 1}-${to} of ${items.length}`}
+                numberOfItemsPerPageList={numberOfItemsPerPageList}
+                numberOfItemsPerPage={itemsPerPage}
+                onItemsPerPageChange={onItemsPerPageChange}
+                showFastPaginationControls
+                selectPageDropdownLabel={'Rows per page'}
+            />
+        </DataTable>
     );
 };
 
