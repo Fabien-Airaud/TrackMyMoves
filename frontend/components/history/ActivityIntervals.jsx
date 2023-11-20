818fe87696c6418475c200b67101a56c6201d76a
FabienTri <airaudfabien@gmail.com>
2023-11-20 15:32:26 -0500

    Display received activity data in JSON

---
+++ frontend/components/history/ActivityIntervals.jsx
@@ -0,0 +1,29 @@
+import { useTheme } from '@react-navigation/native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
+
+const ActivityIntervals = ({ navigation, route: { params: { activity } } }) => {
+    // Style variables
+    const { colors, fontSizes } = useTheme();
+    const styles = StyleSheet.create({
+        page: {
+            minHeight: '100%',
+            width: '100%',
+            padding: '5%',
+            backgroundColor: colors.background
+        },
+        text: {
+            textAlign: 'center',
+            margin: 10,
+            color: colors.text,
+            fontSize: fontSizes.sm
+        }
+    });
+
+    return (
+        <ScrollView style={styles.page}>
+            <Text style={styles.text}>{JSON.stringify(activity)}</Text>
+        </ScrollView>
+    );
+};
+
+export default ActivityIntervals;
