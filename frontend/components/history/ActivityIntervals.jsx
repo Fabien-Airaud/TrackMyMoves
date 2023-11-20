91574f4dbe46a1c091b4f3651e5c8273287d63ca
FabienTri <airaudfabien@gmail.com>
2023-11-20 17:43:39 -0500

    Clarity of code

--- frontend/components/history/ActivityIntervals.jsx
+++ frontend/components/history/ActivityIntervals.jsx
@@ -37,44 +37,44 @@ const ActivityIntervals = ({ navigation, route: { params: { intervals } } }) => 
 
     return (
         <ScrollView horizontal={true}>
-        <DataTable>
-            <DataTable.Header>
-                <DataTable.Title style={[styles.cell, styles.number]}>#</DataTable.Title>
-                <DataTable.Title style={[styles.cell, styles.date]}>Start date</DataTable.Title>
-                <DataTable.Title style={[styles.cell, styles.time]}>Time</DataTable.Title>
-                <DataTable.Title style={[styles.cell, styles.time]}>Total time</DataTable.Title>
-            </DataTable.Header>
+            <DataTable>
+                <DataTable.Header>
+                    <DataTable.Title style={[styles.cell, styles.number]}>#</DataTable.Title>
+                    <DataTable.Title style={[styles.cell, styles.date]}>Start date</DataTable.Title>
+                    <DataTable.Title style={[styles.cell, styles.time]}>Time</DataTable.Title>
+                    <DataTable.Title style={[styles.cell, styles.time]}>Total time</DataTable.Title>
+                </DataTable.Header>
 
-            {intervals.slice(from, to).map((interval, index) => {
-                // Get times in string formatted
-                const time = formatTime(interval.endTime - interval.startTime, false);
-                const totalTime = formatTime(interval.endTime, false);
+                {intervals.slice(from, to).map((interval, index) => {
+                    // Get times in string formatted
+                    const time = formatTime(interval.endTime - interval.startTime, false);
+                    const totalTime = formatTime(interval.endTime, false);
 
-                // Get start date in string
-                const startDate = new Date(interval.startDate).toLocaleString();
+                    // Get start date in string
+                    const startDate = new Date(interval.startDate).toLocaleString();
 
-                return (
-                    <DataTable.Row key={index}>
-                        <DataTable.Cell style={[styles.cell, styles.number]}>{index + from + 1}</DataTable.Cell>
-                        <DataTable.Cell style={[styles.cell, styles.date]}>{startDate}</DataTable.Cell>
-                        <DataTable.Cell style={[styles.cell, styles.time]}>{time}</DataTable.Cell>
-                        <DataTable.Cell style={[styles.cell, styles.time]}>{totalTime}</DataTable.Cell>
-                    </DataTable.Row>
-                );
-            })}
+                    return (
+                        <DataTable.Row key={index}>
+                            <DataTable.Cell style={[styles.cell, styles.number]}>{index + from + 1}</DataTable.Cell>
+                            <DataTable.Cell style={[styles.cell, styles.date]}>{startDate}</DataTable.Cell>
+                            <DataTable.Cell style={[styles.cell, styles.time]}>{time}</DataTable.Cell>
+                            <DataTable.Cell style={[styles.cell, styles.time]}>{totalTime}</DataTable.Cell>
+                        </DataTable.Row>
+                    );
+                })}
 
-            <DataTable.Pagination
-                page={page}
-                numberOfPages={Math.ceil(intervals.length / itemsPerPage)}
-                onPageChange={(page) => setPage(page)}
-                label={`${from + 1}-${to} of ${intervals.length}`}
-                numberOfItemsPerPageList={numberOfItemsPerPageList}
-                numberOfItemsPerPage={itemsPerPage}
-                onItemsPerPageChange={onItemsPerPageChange}
-                showFastPaginationControls
-                selectPageDropdownLabel={'Rows per page'}
-            />
-        </DataTable>
+                <DataTable.Pagination
+                    page={page}
+                    numberOfPages={Math.ceil(intervals.length / itemsPerPage)}
+                    onPageChange={(page) => setPage(page)}
+                    label={`${from + 1}-${to} of ${intervals.length}`}
+                    numberOfItemsPerPageList={numberOfItemsPerPageList}
+                    numberOfItemsPerPage={itemsPerPage}
+                    onItemsPerPageChange={onItemsPerPageChange}
+                    showFastPaginationControls
+                    selectPageDropdownLabel={'Rows per page'}
+                />
+            </DataTable>
         </ScrollView>
     );
 };
