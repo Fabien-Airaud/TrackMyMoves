70706b449c7cad0ca9a222ba79478e7cc7d8f61e
FabienTri <airaudfabien@gmail.com>
2023-11-23 16:49:57 -0500

    Add create user action

--- backend/track_my_moves/views.py
+++ backend/track_my_moves/views.py
@@ -122,6 +122,13 @@ class UserViewSet(viewsets.ViewSet):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ActivityViewSet(viewsets.ViewSet):
     def list(self, request):
