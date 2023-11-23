3066b89515c2d11460d00f54e0576473badd412c
FabienTri <airaudfabien@gmail.com>
2023-11-23 16:47:33 -0500

    Make password write_only

--- backend/track_my_moves/serializers.py
+++ backend/track_my_moves/serializers.py
@@ -17,4 +17,8 @@ class ActivitySerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "email"]
+        fields = ["id", "email", "password"]
+        extra_kwargs = {'password': {'write_only': True}}
+    
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
