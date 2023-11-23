29d9ec577c5349431f57cc465294118741ba4f6d
FabienTri <airaudfabien@gmail.com>
2023-11-23 17:59:05 -0500

    Add retrieve for account

--- backend/track_my_moves/views.py
+++ backend/track_my_moves/views.py
@@ -169,6 +169,11 @@ class AccountViewSet(viewsets.ViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def retrieve(self, request, pk):
+        account = Account.objects.get(id=pk)
+        serializer = AccountSerializer(account)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class ActivityViewSet(viewsets.ViewSet):
