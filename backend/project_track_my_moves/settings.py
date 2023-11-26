365f674e3def8f28eaf3cc188cf6a4ae053474a6
FabienTri <airaudfabien@gmail.com>
2023-11-26 18:44:03 -0500

    Remove unused static dirs

--- backend/project_track_my_moves/settings.py
+++ backend/project_track_my_moves/settings.py
@@ -136,10 +136,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "track_my_moves/static")
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
