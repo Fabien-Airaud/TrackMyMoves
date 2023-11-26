652ddbd6fbd7323f62caad3b06558d3e079dc109
FabienTri <airaudfabien@gmail.com>
2023-11-25 20:33:06 -0500

    Try to resolve

--- backend/project_track_my_moves/settings.py
+++ backend/project_track_my_moves/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'project_track_my_moves.wsgi.application'
+WSGI_APPLICATION = 'project_track_my_moves.wsgi.app'
 
 
 # Database
--- backend/project_track_my_moves/wsgi.py
+++ backend/project_track_my_moves/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_track_my_moves.settings')
 
-application = get_wsgi_application()
+app = get_wsgi_application()
