ba3c12a4aa9efb17bdc496bbede12e91a8e991dd
FabienTri <airaudfabien@gmail.com>
2023-11-29 10:24:35 -0500

    Static problem corrected in server

--- backend/project_track_my_moves/urls.py
+++ backend/project_track_my_moves/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('TrackMyMoves/', include('track_my_moves.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
