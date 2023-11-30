50040241c7fa083af3364cea77e22a0d0d0bf8ff
FabienTri <airaudfabien@gmail.com>
2023-11-29 23:06:47 -0500

    Add function to register

--- frontend/components/APIFunctions.jsx
+++ frontend/components/APIFunctions.jsx
@@ -1,3 +1,5 @@
+const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+
 export const dateToStringAPIDate = (date) => {
     let stringAPIDate = "";
     new Date().getUTCDate;
@@ -7,4 +9,31 @@ export const dateToStringAPIDate = (date) => {
     stringAPIDate += "-";
     stringAPIDate += date.getDate();
     return stringAPIDate;
+}
+
+export const registerAPI = async (email, password, first_name, last_name, birthdate, height, weight, country) => {
+    let helpers = undefined;
+
+    const response = await fetch(apiUrl + "/register", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            user: {
+                email: email,
+                password: password
+            },
+            first_name: first_name,
+            last_name: last_name,
+            birthdate: dateToStringAPIDate(birthdate),
+            height: height,
+            weight: weight,
+            country: country
+        }),
+    });
+
+    if (!response.ok) helpers = await response.json();
+    return helpers;
 }
