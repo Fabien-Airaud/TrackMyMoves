9fec3f20d81b6a2ff02c73e918350ffd3b0fb15e
FabienTri <airaudfabien@gmail.com>
2023-11-30 15:51:34 -0500

    Use api to log in if token remembered

--- frontend/components/navigation/AppNav.jsx
+++ frontend/components/navigation/AppNav.jsx
@@ -2,41 +2,38 @@ import * as FileSystem from 'expo-file-system';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { logInAccount } from '../../redux/logInSlice';
+import { newAccount } from '../../redux/apiAccountSlice';
+import { retrieveAccountAPI } from '../APIFunctions';
 import AppTab from './AppTab';
 import HomeStack from './HomeStack';
 
 const AppNav = () => {
-    // Variables using hooks
     const dispatch = useDispatch();
-    const accounts = useSelector((state) => state.accounts);
 
     useEffect(() => {
-        // log in if has a log in file in local
-        const dispatchLogIn = async () => {
+        // log in if has a logIn file in local
+        const logIn = async () => {
             const logInFileURI = FileSystem.documentDirectory + 'logIn.json';
             const fileInfo = await FileSystem.getInfoAsync(logInFileURI);
 
-            // if log in file exists and there are at least 1 account
-            if (fileInfo.exists && !fileInfo.isDirectory && !(accounts.length === 0)) {
-                const accountId = await FileSystem.readAsStringAsync(logInFileURI);
+            // if log in file exists
+            if (fileInfo.exists && !fileInfo.isDirectory) {
+                const accountToken = await FileSystem.readAsStringAsync(logInFileURI);
 
-                // Try to find the relative account for the accountId given
-                const account = accounts.find(account => account.id === accountId);
-
-                // If found an account, then dispatch it to log in
-                if (account) dispatch(logInAccount(account));
+                // Try to log in with the token or delete logIn file
+                const account = await retrieveAccountAPI(accountToken, 0);
+                if (account) dispatch(newAccount({ token: accountToken, account: account }));
                 else await FileSystem.deleteAsync(logInFileURI);
             }
         };
 
-        dispatchLogIn().catch(console.error);;
+        logIn().catch(console.error);;
     }, []);
 
     // Logged account stored in redux
-    const loggedAccount = useSelector((state) => state.logIn);
+    const loggedAccount = useSelector((state) => state.apiAccount);
 
-    if (loggedAccount) return <AppTab />;
+    if (loggedAccount.token) return <AppTab />;
     else return <HomeStack />;
 };
 
