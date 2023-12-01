1216f62c6dacc5ec7df258d23028bcacb19d197d
FabienTri <airaudfabien@gmail.com>
2023-12-01 11:53:13 -0500

    Update birthdate with API and add helper

--- frontend/components/profile/BirthdateProfile.jsx
+++ frontend/components/profile/BirthdateProfile.jsx
@@ -3,15 +3,20 @@ import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { IconButton } from 'react-native-paper';
 import { DatePickerInput } from 'react-native-paper-dates';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { updateBirthdate } from '../../redux/accountSlice';
-import { updateLogBirthdate } from '../../redux/logInSlice';
+import { changeAccount } from '../../redux/apiAccountSlice';
+import { dateToStringAPIDate, patchAccountAPI, stringAPIDateToDate } from '../APIFunctions';
+import Helper from '../Helper';
 
-const BirthdateProfile = ({ id, data }) => {
+const BirthdateProfile = () => {
+    // Logged account stored in redux
+    const apiAccount = useSelector((state) => state.apiAccount);
+
     // state variables
     const [edit, setEdit] = useState(false);
-    const [value, setValue] = useState(new Date(data));
+    const [value, setValue] = useState(stringAPIDateToDate(apiAccount.account.birthdate));
+    const [helpers, setHelpers] = useState(undefined);
 
     // Style variables
     const { colors, fontSizes } = useTheme();
@@ -21,6 +26,10 @@ const BirthdateProfile = ({ id, data }) => {
             justifyContent: 'start',
             alignItems: 'center',
             width: '100%',
+        },
+        colView: {
+            alignItems: 'start',
+            width: '100%'
         },
         text: {
             color: colors.text,
@@ -34,50 +43,58 @@ const BirthdateProfile = ({ id, data }) => {
         }
     });
 
+
     // Dispatch account
     const dispatch = useDispatch();
 
-    const dispatchBirthdate = () => {
+    const updateBirthdate = () => {
         // Convert to string
-        const birthdate = value.toISOString();
+        const birthdate = dateToStringAPIDate(value);
 
-        if (data !== birthdate) {
-            dispatch(
-                updateBirthdate({
-                    id: id,
-                    birthdate: birthdate
+        if (apiAccount.account.birthdate !== birthdate) {
+            patchAccountAPI(apiAccount.token, apiAccount.account.id, { birthdate: birthdate })
+                .then(data => {
+                    if (data.ok) {
+                        setHelpers(undefined);
+                        dispatch(changeAccount(data.ok));
+                    }
+                    else {
+                        setHelpers(data.helpers);
+                    }
                 })
-            );
-            dispatch(updateLogBirthdate(birthdate));
+                .catch(console.error);
         }
         setEdit(false);
     };
 
     return (
-        <View style={styles.view}>
-            <DatePickerInput
-                locale='en'
-                label={edit ? 'Birthdate' : ''}
-                withDateFormatInLabel={edit}
-                value={value}
-                disabled={!edit}
-                onChange={(date) => setValue(date)}
-                validRange={{ endDate: Date.now() }}
-                inputMode='start'
-                textColor={colors.text}
-                theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
-                iconColor={colors.placeholder}
-                style={styles.input}
-            />
-            <View style={[styles.view, { paddingHorizontal: 5, minWidth: 3 * fontSizes.sm, maxWidth: '25%' }]}>
-                {edit ?
-                    <>
-                        <IconButton icon='check-bold' onPress={() => dispatchBirthdate()} iconColor={colors.text} containerColor={colors.success} size={fontSizes.sm} />
-                        <IconButton icon='close-thick' onPress={() => { setEdit(false); setValue(new Date(data)); }} iconColor={colors.text} containerColor={colors.error} size={fontSizes.sm} />
-                    </>
-                    : <IconButton icon='pencil' onPress={() => setEdit(true)} iconColor={colors.text} containerColor={colors.primary} size={fontSizes.sm} />
-                }
+        <View style={styles.colView}>
+            <View style={styles.view}>
+                <DatePickerInput
+                    locale='en'
+                    label={edit ? 'Birthdate' : ''}
+                    withDateFormatInLabel={edit}
+                    value={value}
+                    disabled={!edit}
+                    onChange={(date) => setValue(date)}
+                    validRange={{ endDate: Date.now() }}
+                    inputMode='start'
+                    textColor={colors.text}
+                    theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
+                    iconColor={colors.placeholder}
+                    style={styles.input}
+                />
+                <View style={[styles.view, { paddingHorizontal: 5, minWidth: 3 * fontSizes.sm, maxWidth: '25%' }]}>
+                    {edit ?
+                        <>
+                            <IconButton icon='check-bold' onPress={updateBirthdate} iconColor={colors.text} containerColor={colors.success} size={fontSizes.sm} />
+                            <IconButton icon='close-thick' onPress={() => { setEdit(false); setValue(stringAPIDateToDate(apiAccount.account.birthdate)); }} iconColor={colors.text} containerColor={colors.error} size={fontSizes.sm} />
+                        </>
+                        : <IconButton icon='pencil' onPress={() => setEdit(true)} iconColor={colors.text} containerColor={colors.primary} size={fontSizes.sm} />
+                    }
+                </View>
             </View>
+            <Helper visible={helpers && helpers.birthdate} message={helpers && helpers.birthdate ? helpers.birthdate : ''} />
         </View>
     );
 };
