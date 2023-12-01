2c2d0aec1d32d359dc932568c547765451b334fb
FabienTri <airaudfabien@gmail.com>
2023-12-01 11:59:50 -0500

    Update height using API and add helpers

--- frontend/components/profile/HeightProfile.jsx
+++ frontend/components/profile/HeightProfile.jsx
@@ -2,15 +2,20 @@ import { useTheme } from '@react-navigation/native';
 import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { IconButton, TextInput } from 'react-native-paper';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { updateHeight } from '../../redux/accountSlice';
-import { updateLogHeight } from '../../redux/logInSlice';
+import { changeAccount } from '../../redux/apiAccountSlice';
+import { patchAccountAPI } from '../APIFunctions';
+import Helper from '../Helper';
 
-const HeightProfile = ({ id, data }) => {
+const HeightProfile = () => {
+    // Logged account stored in redux
+    const apiAccount = useSelector((state) => state.apiAccount);
+
     // state variables
     const [edit, setEdit] = useState(false);
-    const [value, setValue] = useState(data);
+    const [value, setValue] = useState(""+apiAccount.account.height);
+    const [helpers, setHelpers] = useState(undefined);
 
     // Style variables
     const { colors, fontSizes } = useTheme();
@@ -20,6 +25,10 @@ const HeightProfile = ({ id, data }) => {
             justifyContent: 'start',
             alignItems: 'center',
             width: '100%',
+        },
+        colView: {
+            alignItems: 'start',
+            width: '100%'
         },
         text: {
             color: colors.text,
@@ -33,45 +42,66 @@ const HeightProfile = ({ id, data }) => {
         }
     });
 
+
     // Dispatch account
     const dispatch = useDispatch();
 
-    const dispatchHeight = () => {
-        if (/^\b\d{1,3}\b$/.test(value) && data !== value) {
-            dispatch(
-                updateHeight({
-                    id: id,
-                    height: value
+    const updateHeight = () => {
+        if (""+apiAccount.account.height !== value) {
+            patchAccountAPI(apiAccount.token, apiAccount.account.id, { height: value })
+                .then(data => {
+                    if (data.ok) {
+                        setHelpers(undefined);
+                        dispatch(changeAccount(data.ok));
+                    }
+                    else {
+                        setHelpers(data.helpers);
+                    }
                 })
-            );
-            dispatch(updateLogHeight(value));
-        } else {
-            setValue(data);
+                .catch(console.error);
         }
         setEdit(false);
     };
 
+    // const dispatchHeight = () => {
+    //     if (/^\b\d{1,3}\b$/.test(value) && data !== value) {
+    //         dispatch(
+    //             updateHeight({
+    //                 id: id,
+    //                 height: value
+    //             })
+    //         );
+    //         dispatch(updateLogHeight(value));
+    //     } else {
+    //         setValue(data);
+    //     }
+    //     setEdit(false);
+    // };
+
     return (
-        <View style={styles.view}>
-            <TextInput
-                label={edit ? 'Height (cm)' : ''}
-                value={value}
-                disabled={!edit}
-                onChangeText={number => setValue(number)}
-                inputMode='numeric'
-                textColor={colors.text}
-                theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
-                style={styles.input}
-            />
-            <View style={[styles.view, { paddingHorizontal: 5, width: 3 * fontSizes.sm }]}>
-                {edit ?
-                    <>
-                        <IconButton icon='check-bold' onPress={() => dispatchHeight()} iconColor={colors.text} containerColor={colors.success} size={fontSizes.sm} />
-                        <IconButton icon='close-thick' onPress={() => { setEdit(false); setValue(data); }} iconColor={colors.text} containerColor={colors.error} size={fontSizes.sm} />
-                    </>
-                    : <IconButton icon='pencil' onPress={() => setEdit(true)} iconColor={colors.text} containerColor={colors.primary} size={fontSizes.sm} />
-                }
+        <View style={styles.colView}>
+            <View style={styles.view}>
+                <TextInput
+                    label={edit ? 'Height (cm)' : ''}
+                    value={value}
+                    disabled={!edit}
+                    onChangeText={number => setValue(number)}
+                    inputMode='numeric'
+                    textColor={colors.text}
+                    theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
+                    style={styles.input}
+                />
+                <View style={[styles.view, { paddingHorizontal: 5, width: 3 * fontSizes.sm }]}>
+                    {edit ?
+                        <>
+                            <IconButton icon='check-bold' onPress={updateHeight} iconColor={colors.text} containerColor={colors.success} size={fontSizes.sm} />
+                            <IconButton icon='close-thick' onPress={() => { setEdit(false); setValue(""+apiAccount.account.height); }} iconColor={colors.text} containerColor={colors.error} size={fontSizes.sm} />
+                        </>
+                        : <IconButton icon='pencil' onPress={() => setEdit(true)} iconColor={colors.text} containerColor={colors.primary} size={fontSizes.sm} />
+                    }
+                </View>
             </View>
+            <Helper visible={helpers && helpers.height} message={helpers && helpers.height ? helpers.height : ''} />
         </View>
     );
 };
