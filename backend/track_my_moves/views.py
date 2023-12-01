154d71914b0175360d239181ea5371033cbbf8a8
FabienTri <airaudfabien@gmail.com>
2023-11-30 21:37:53 -0500

    Possibility to update user when authenticated

--- backend/track_my_moves/views.py
+++ backend/track_my_moves/views.py
@@ -157,10 +157,12 @@ def logOutAPIViewDeco(request):
 
 
 class UserViewSet(viewsets.ViewSet):
-    def list(self, request):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    permission_classes = [IsAuthenticated]
+
+    # def list(self, request):
+    #     users = User.objects.all()
+    #     serializer = UserSerializer(users, many=True)
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
     
     # def create(self, request):
     #     serializer = UserSerializer(data=request.data)
@@ -174,21 +176,21 @@ class UserViewSet(viewsets.ViewSet):
     #     serializer = UserSerializer(user)
     #     return Response(serializer.data, status=status.HTTP_200_OK)
     
-    # def update(self, request, pk):
-    #     user = User.objects.get(id=pk)
-    #     serializer = UserSerializer(user, data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_200_OK)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def update(self, request, pk):
+        user = User.objects.get(id=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    # def partial_update(self, request, pk):
-    #     user = User.objects.get(id=pk)
-    #     serializer = UserSerializer(user, data=request.data, partial=True)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_200_OK)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def partial_update(self, request, pk):
+        user = User.objects.get(id=pk)
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     # def destroy(self, request, pk):
     #     user = User.objects.get(id=pk)
