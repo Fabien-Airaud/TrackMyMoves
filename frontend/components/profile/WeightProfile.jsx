d8d5031ffd2856796740b2b8e8bca21651a288a5
FabienTri <airaudfabien@gmail.com>
2023-12-01 12:03:55 -0500

    Update weight using API and add helpers

--- frontend/components/profile/WeightProfile.jsx
+++ frontend/components/profile/WeightProfile.jsx
@@ -2,15 +2,20 @@ import { useTheme } from '@react-navigation/native';
 import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { IconButton, TextInput } from 'react-native-paper';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { updateWeight } from '../../redux/accountSlice';
-import { updateLogWeight } from '../../redux/logInSlice';
+import { changeAccount } from '../../redux/apiAccountSlice';
+import { patchAccountAPI } from '../APIFunctions';
+import Helper from '../Helper';
 
-const WeightProfile = ({ id, data }) => {
+const WeightProfile = () => {
+    // Logged account stored in redux
+    const apiAccount = useSelector((state) => state.apiAccount);
+
     // state variables
     const [edit, setEdit] = useState(false);
-    const [value, setValue] = useState(data);
+    const [value, setValue] = useState("" + apiAccount.account.weight);
+    const [helpers, setHelpers] = useState(undefined);
 
     // Style variables
     const { colors, fontSizes } = useTheme();
@@ -20,6 +25,10 @@ const WeightProfile = ({ id, data }) => {
             justifyContent: 'start',
             alignItems: 'center',
             width: '100%',
+        },
+        colView: {
+            alignItems: 'start',
+            width: '100%'
         },
         text: {
             color: colors.text,
@@ -33,45 +42,51 @@ const WeightProfile = ({ id, data }) => {
         }
     });
 
+    
     // Dispatch account
     const dispatch = useDispatch();
 
-    const dispatchWeight = () => {
-        if (/^\b\d+[.,]?\d*\b$/.test(value) && value < 1000 && data !== value) {
-            dispatch(
-                updateWeight({
-                    id: id,
-                    weight: value
+    const updateWeight = () => {
+        if ("" + apiAccount.account.weight !== value) {
+            patchAccountAPI(apiAccount.token, apiAccount.account.id, { weight: value })
+                .then(data => {
+                    if (data.ok) {
+                        setHelpers(undefined);
+                        dispatch(changeAccount(data.ok));
+                    }
+                    else {
+                        setHelpers(data.helpers);
+                    }
                 })
-            );
-            dispatch(updateLogWeight(value));
-        } else {
-            setValue(data);
+                .catch(console.error);
         }
         setEdit(false);
     };
 
     return (
-        <View style={styles.view}>
-            <TextInput
-                label={edit ? 'Weight (kg)' : ''}
-                value={value}
-                disabled={!edit}
-                onChangeText={number => setValue(number)}
-                inputMode='numeric'
-                textColor={colors.text}
-                theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
-                style={styles.input}
-            />
-            <View style={[styles.view, { paddingHorizontal: 5, width: 3 * fontSizes.sm }]}>
-                {edit ?
-                    <>
-                        <IconButton icon='check-bold' onPress={() => dispatchWeight()} iconColor={colors.text} containerColor={colors.success} size={fontSizes.sm} />
-                        <IconButton icon='close-thick' onPress={() => { setEdit(false); setValue(data); }} iconColor={colors.text} containerColor={colors.error} size={fontSizes.sm} />
-                    </>
-                    : <IconButton icon='pencil' onPress={() => setEdit(true)} iconColor={colors.text} containerColor={colors.primary} size={fontSizes.sm} />
-                }
+        <View style={styles.colView}>
+            <View style={styles.view}>
+                <TextInput
+                    label={edit ? 'Weight (kg)' : ''}
+                    value={value}
+                    disabled={!edit}
+                    onChangeText={number => setValue(number)}
+                    inputMode='numeric'
+                    textColor={colors.text}
+                    theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
+                    style={styles.input}
+                />
+                <View style={[styles.view, { paddingHorizontal: 5, width: 3 * fontSizes.sm }]}>
+                    {edit ?
+                        <>
+                            <IconButton icon='check-bold' onPress={updateWeight} iconColor={colors.text} containerColor={colors.success} size={fontSizes.sm} />
+                            <IconButton icon='close-thick' onPress={() => { setEdit(false); setValue("" + apiAccount.account.weight); }} iconColor={colors.text} containerColor={colors.error} size={fontSizes.sm} />
+                        </>
+                        : <IconButton icon='pencil' onPress={() => setEdit(true)} iconColor={colors.text} containerColor={colors.primary} size={fontSizes.sm} />
+                    }
+                </View>
             </View>
+            <Helper visible={helpers && helpers.weight} message={helpers && helpers.weight ? helpers.weight : ''} />
         </View>
     );
 };
