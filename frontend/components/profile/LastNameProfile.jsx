0048fb679dc54dc1949cbe497585e01146219d4e
FabienTri <airaudfabien@gmail.com>
2023-11-30 21:20:43 -0500

    Use update account to change last name and add helper

--- frontend/components/profile/LastNameProfile.jsx
+++ frontend/components/profile/LastNameProfile.jsx
@@ -2,15 +2,20 @@ import { useTheme } from '@react-navigation/native';
 import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { IconButton, TextInput } from 'react-native-paper';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { updateLastName } from '../../redux/accountSlice';
-import { updateLogLastName } from '../../redux/logInSlice';
+import { changeAccount } from '../../redux/apiAccountSlice';
+import { patchAccountAPI } from '../APIFunctions';
+import Helper from '../Helper';
 
-const LastNameProfile = ({ id, data }) => {
+const LastNameProfile = () => {
+    // Logged account stored in redux
+    const apiAccount = useSelector((state) => state.apiAccount);
+
     // state variables
     const [edit, setEdit] = useState(false);
-    const [value, setValue] = useState(data);
+    const [value, setValue] = useState(apiAccount.account.last_name);
+    const [helpers, setHelpers] = useState(undefined);
 
     // Style variables
     const { colors, fontSizes } = useTheme();
@@ -20,6 +25,10 @@ const LastNameProfile = ({ id, data }) => {
             justifyContent: 'start',
             alignItems: 'center',
             width: '100%',
+        },
+        colView: {
+            alignItems: 'start',
+            width: '100%'
         },
         text: {
             color: colors.text,
@@ -36,40 +45,47 @@ const LastNameProfile = ({ id, data }) => {
     // Dispatch account
     const dispatch = useDispatch();
 
-    const dispatchLastName = () => {
-        if (data !== value) {
-            dispatch(
-                updateLastName({
-                    id: id,
-                    lastName: value
+    const updateLastName = () => {
+        if (apiAccount.account.last_name !== value) {
+            patchAccountAPI(apiAccount.token, apiAccount.account.id, { last_name: value })
+                .then(data => {
+                    if (data.ok) {
+                        setHelpers(undefined);
+                        dispatch(changeAccount(data.ok));
+                    }
+                    else {
+                        setHelpers(data.helpers);
+                    }
                 })
-            );
-            dispatch(updateLogLastName(value));
+                .catch(console.error);
         }
         setEdit(false);
     };
 
     return (
-        <View style={styles.view}>
-            <TextInput
-                label={edit ? 'Last name' : ''}
-                value={value}
-                disabled={!edit}
-                onChangeText={text => setValue(text)}
-                inputMode={'text'}
-                textColor={colors.text}
-                theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
-                style={styles.input}
-            />
-            <View style={[styles.view, { paddingHorizontal: 5, width: 3 * fontSizes.sm }]}>
-                {edit ?
-                    <>
-                        <IconButton icon='check-bold' onPress={() => dispatchLastName()} iconColor={colors.text} containerColor={colors.success} size={fontSizes.sm} />
-                        <IconButton icon='close-thick' onPress={() => { setEdit(false); setValue(data); }} iconColor={colors.text} containerColor={colors.error} size={fontSizes.sm} />
-                    </>
-                    : <IconButton icon='pencil' onPress={() => setEdit(true)} iconColor={colors.text} containerColor={colors.primary} size={fontSizes.sm} />
-                }
+        <View style={styles.colView}>
+            <View style={styles.view}>
+                <TextInput
+                    label={edit ? 'Last name' : ''}
+                    value={value}
+                    disabled={!edit}
+                    onChangeText={text => setValue(text)}
+                    inputMode={'text'}
+                    textColor={colors.text}
+                    theme={{ colors: { primary: colors.primary, onSurfaceVariant: colors.placeholder } }}
+                    style={styles.input}
+                />
+                <View style={[styles.view, { paddingHorizontal: 5, width: 3 * fontSizes.sm }]}>
+                    {edit ?
+                        <>
+                            <IconButton icon='check-bold' onPress={updateLastName} iconColor={colors.text} containerColor={colors.success} size={fontSizes.sm} />
+                            <IconButton icon='close-thick' onPress={() => { setEdit(false); setValue(apiAccount.account.last_name); }} iconColor={colors.text} containerColor={colors.error} size={fontSizes.sm} />
+                        </>
+                        : <IconButton icon='pencil' onPress={() => setEdit(true)} iconColor={colors.text} containerColor={colors.primary} size={fontSizes.sm} />
+                    }
+                </View>
             </View>
+            <Helper visible={helpers && helpers.last_name} message={helpers && helpers.last_name ? helpers.last_name : ''} />
         </View>
     );
 };
