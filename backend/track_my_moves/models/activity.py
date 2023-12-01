deb11142e43e2899f7c821941861abea8157aa1a
FabienTri <airaudfabien@gmail.com>
2023-12-01 14:52:08 -0500

    Change activity to use ActivityType

--- backend/track_my_moves/models/activity.py
+++ backend/track_my_moves/models/activity.py
@@ -1,21 +1,10 @@
 from django.db import models
-from django.core.exceptions import ValidationError
-
-class ActivityTypes(models.TextChoices):
-    WALKING = "walking"
-    RUNNING = "running"
-    CYCLING = "cycling"
-    CARDIO = "cardio"
-
-def validate_activity_type(value):
-    if not (value in ActivityTypes.values):
-        raise ValidationError("Value '{}' is not an activity type".format(value))
 
 class Activity(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE)
-    activity_type = models.CharField(max_length=50, choices=ActivityTypes.choices, validators=[validate_activity_type])
+    activity_type = models.ForeignKey("ActivityType", on_delete=models.CASCADE)
     start_date = models.DateTimeField(auto_now=False, auto_now_add=False)
-    end_date = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
+    end_date = models.DateTimeField(auto_now=False, auto_now_add=False)
     
     def save(self, *args, **kwargs):
         self.full_clean()
