b97e704cbccf40fd0d513562c9f456a4381b1272
FabienTri <airaudfabien@gmail.com>
2023-12-01 13:47:22 -0500

    Create activity type

---
+++ backend/track_my_moves/models/activity_type.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class ActivityType(models.Model):
+    value = models.CharField(max_length=50, unique=True)
+    label = models.CharField(max_length=100, unique=True)
+    leadingIcon = models.CharField(max_length=100, default="timer")
+    
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super(ActivityType, self).save(*args, **kwargs)
