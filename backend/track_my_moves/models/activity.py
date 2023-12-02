fb50da7689c802e99d6810d5dae3b47f79306197
FabienTri <airaudfabien@gmail.com>
2023-12-02 10:46:28 -0500

    Set default activity type for activities

--- backend/track_my_moves/models/activity.py
+++ backend/track_my_moves/models/activity.py
@@ -1,8 +1,12 @@
 from django.db import models
+from .activity_type import ActivityType
+
+def getAIActivityType():
+    return ActivityType.objects.get_or_create(value="ai", label="AI guess", leadingIcon="cloud-braces")
 
 class Activity(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE)
-    activity_type = models.ForeignKey("ActivityType", on_delete=models.CASCADE)
+    activity_type = models.ForeignKey("ActivityType", default=getAIActivityType, on_delete=models.SET_DEFAULT)
     start_date = models.DateTimeField(auto_now=False, auto_now_add=False)
     end_date = models.DateTimeField(auto_now=False, auto_now_add=False)
     
