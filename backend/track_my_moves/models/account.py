fb7a529b98abf2070cd80566fa10dd97584656bd
FabienTri <airaudfabien@gmail.com>
2023-12-02 12:03:11 -0500

    Add constraint to height and weight

--- backend/track_my_moves/models/account.py
+++ backend/track_my_moves/models/account.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 class Account(models.Model):
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="height_less_than_3m",
+                check=models.Q(height__lt=300)
+            ),
+            models.CheckConstraint(
+                name="weight_less_than_1000kg",
+                check=models.Q(weight__lt=1000)
+            )
+        ]
+
     user = models.OneToOneField("User", on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
