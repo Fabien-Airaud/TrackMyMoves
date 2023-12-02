b2244f7f229f26bb8aa8e062910d66d4b77c7810
FabienTri <airaudfabien@gmail.com>
2023-12-02 10:59:17 -0500

    Create activity interval

---
+++ backend/track_my_moves/models/activity_interval.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class ActivityInterval(models.Model):
+    activity = models.ForeignKey("Activity", on_delete=models.CASCADE)
+    start_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
+    start_time = models.BigIntegerField()
+    end_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
+    end_time = models.BigIntegerField()
+    
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super(ActivityInterval, self).save(*args, **kwargs)
