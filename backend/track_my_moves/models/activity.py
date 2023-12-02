088aa36ce24c93d8de3d715a0203ea43c2a87b44
FabienTri <airaudfabien@gmail.com>
2023-12-02 11:59:50 -0500

    Add constraint between datetimes

--- backend/track_my_moves/models/activity.py
+++ backend/track_my_moves/models/activity.py
@@ -5,6 +5,14 @@ def getAIActivityType():
     return ActivityType.objects.get_or_create(value="ai", label="AI guess", leadingIcon="cloud-braces")
 
 class Activity(models.Model):
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="start_datetime_less_than_end_datetime",
+                check=models.Q(start_datetime__lt=models.F("end_datetime")),
+            )
+        ]
+    
     user = models.ForeignKey("User", on_delete=models.CASCADE)
     activity_type = models.ForeignKey("ActivityType", default=getAIActivityType, on_delete=models.SET_DEFAULT)
     start_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
