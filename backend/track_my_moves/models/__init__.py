af158ed438994d0859556a93d6c22596b918aa1f
FabienTri <airaudfabien@gmail.com>
2023-12-02 10:59:30 -0500

    Add activity interval in models

--- backend/track_my_moves/models/__init__.py
+++ backend/track_my_moves/models/__init__.py
@@ -1,4 +1,5 @@
-from .activity import Activity
 from .user import User
 from .account import Account
+from .activity import Activity
 from .activity_type import ActivityType
+from .activity_interval import ActivityInterval
