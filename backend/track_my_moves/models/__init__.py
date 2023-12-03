f0c36abc7371cd058cfd3fd2e838f547fed2c546
FabienTri <airaudfabien@gmail.com>
2023-12-03 09:09:05 -0500

    Add SensorsInterval in models

--- backend/track_my_moves/models/__init__.py
+++ backend/track_my_moves/models/__init__.py
@@ -3,3 +3,4 @@ from .account import Account
 from .activity import Activity
 from .activity_type import ActivityType
 from .activity_interval import ActivityInterval
+from .sensors_interval import SensorsInterval
