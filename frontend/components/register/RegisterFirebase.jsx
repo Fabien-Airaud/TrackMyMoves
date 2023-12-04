dba906e0cd6a006fa8621199ffcb6c83539787f1
FabienTri <airaudfabien@gmail.com>
2023-12-04 18:11:21 -0500

    Add helpers when registering with firebase

--- frontend/components/register/RegisterFirebase.jsx
+++ frontend/components/register/RegisterFirebase.jsx
@@ -9,14 +9,14 @@ import { auth } from '../../firebaseConfig';
 import Helper from '../Helper';
 import Input from '../Input';
 
-const RegisterFirebase = ({ navigation }) => {
+const RegisterFirebase = () => {
     // Input variables
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [securedPassword, setSecuredPassword] = useState(true);
     const [confirmPassword, setConfirmPassword] = useState('');
     const [securedConfirmPassword, setSecuredConfirmPassword] = useState(true);
-    // const [helpers, setHelpers] = useState(undefined);
+    const [helpers, setHelpers] = useState(undefined);
 
     // Style variables
     const { colors, fontSizes } = useTheme();
@@ -40,10 +40,14 @@ const RegisterFirebase = ({ navigation }) => {
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 // Signed up
+                setHelpers(undefined);
                 const user = userCredential.user;
                 console.log("Registered: " + JSON.stringify(user));
             })
-            .catch(console.error);
+            .catch((error) => {
+                setHelpers(error.code);
+                console.log(JSON.stringify(error.message));
+            });
     };
 
     return (
@@ -51,15 +55,13 @@ const RegisterFirebase = ({ navigation }) => {
             <Text style={styles.title}>Register with Firebase</Text>
 
             <Input label='Email address' placeholder='Enter your email address' onChangeText={text => setEmail(text)} inputMode='email' />
-            {/* <Helper visible={helpers?.user?.email != undefined} message={helpers?.user?.email ? helpers?.user?.email[0] : ""} /> */}
 
             <Input label='Password' placeholder='Enter your password' secureTextEntry={securedPassword} onChangeText={text => setPassword(text)} inputMode='text' right={<TextInput.Icon icon={securedPassword ? 'eye' : 'eye-off'} onPress={() => setSecuredPassword(!securedPassword)} color={colors.placeholder} />} />
-            {/* <Helper visible={helpers?.user?.password != undefined} message={helpers?.user?.password ? helpers?.user?.password[0] : ""} /> */}
 
             <Input label='Confirm password' placeholder='Confirm your password' secureTextEntry={securedConfirmPassword} onChangeText={text => setConfirmPassword(text)} inputMode='text' right={<TextInput.Icon icon={securedConfirmPassword ? 'eye' : 'eye-off'} onPress={() => setSecuredConfirmPassword(!securedConfirmPassword)} color={colors.placeholder} />} />
 
             <Button title='Register with Firebase' disabled={disableRegister()} onPress={register} size='md' radius='sm' titleStyle={{ fontWeight: 'bold' }} disabledTitleStyle={{ color: colors.placeholder }} disabledStyle={{ backgroundColor: colors.inputFill }} containerStyle={{ marginHorizontal: '5%', marginTop: '5%' }} />
-            <Helper visible={disableRegister()} message={(password !== confirmPassword) ? 'password and password confirmation should be the same' : 'all the inputs should be correctly filled.'} justifyContent='center' />
+            <Helper visible={disableRegister() || helpers} message={(password !== confirmPassword) ? 'password and password confirmation should be the same' : helpers ? helpers : 'all the inputs should be correctly filled.'} justifyContent='center' />
         </View>
     );
 };
