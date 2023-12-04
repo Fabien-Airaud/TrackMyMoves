66cb95245f2c55de30c0b3990620c5f8e1e5162f
FabienTri <airaudfabien@gmail.com>
2023-12-03 19:45:55 -0500

    Change to get activity from store

--- frontend/components/history/ActivityGeneral.jsx
+++ frontend/components/history/ActivityGeneral.jsx
@@ -1,16 +1,19 @@
 import { useTheme } from '@react-navigation/native';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Avatar, Divider, List, Surface } from 'react-native-paper';
+import { useSelector } from 'react-redux';
 
-import ActivityTypes from '../move/ActivityTypes';
 import { formatTime } from '../move/FormatTime';
 
-const ActivityGeneral = ({ navigation, route: { params } }) => {
+const ActivityGeneral = () => {
+    // Activity infos stored in redux
+    const activity = useSelector((state) => state.apiActivityInfos);
+
     // Activity type
-    const activityType = ActivityTypes.find((activityType) => activityType.value == params.activityType);
-    const startDate = new Date(params.startDate);
-    const endDate = new Date(params.endDate);
-    const totalTime = formatTime(params.intervals[params.intervals.length - 1].endTime, false);
+    const activityType = activity.activity_type;
+    const startDate = new Date(activity.start_datetime);
+    const endDate = new Date(activity.end_datetime);
+    const totalTime = formatTime(activity.total_time, false);
     const elapsedTime = formatTime(endDate - startDate, false);
 
     // Style variables
@@ -33,10 +36,10 @@ const ActivityGeneral = ({ navigation, route: { params } }) => {
     return (
         <ScrollView style={styles.page}>
             <View style={{ flexDirection: 'row' }}>
-                <Avatar.Icon icon={activityType.leadingIcon} size={100} color={colors.text} style={{ backgroundColor: colors.background }} />
+                <Avatar.Icon icon={activityType.leading_icon} size={100} color={colors.text} style={{ backgroundColor: colors.background }} />
                 <View style={{ justifyContent: 'space-evenly' }}>
                     <Text style={[styles.text, { fontSize: fontSizes.xl }]}>{activityType.label}</Text>
-                    <Text style={styles.text}>{new Date(params.startDate).toDateString()}</Text>
+                    <Text style={styles.text}>{startDate.toDateString()}</Text>
                 </View>
             </View>
 
@@ -45,25 +48,25 @@ const ActivityGeneral = ({ navigation, route: { params } }) => {
                 <Surface mode='flat' elevation={1} style={{ backgroundColor: colors.surface }}>
                     <List.Item
                         title={startDate.toLocaleTimeString()}
-                        description="Start time"
+                        description="Start"
                         titleStyle={styles.listTitle}
                     />
                     <Divider />
                     <List.Item
                         title={endDate.toLocaleTimeString()}
-                        description="End time"
+                        description="End"
                         titleStyle={styles.listTitle}
                     />
                     <Divider />
                     <List.Item
                         title={totalTime}
-                        description="Total time"
+                        description="Total"
                         titleStyle={styles.listTitle}
                     />
                     <Divider />
                     <List.Item
                         title={elapsedTime}
-                        description="Elapsed time"
+                        description="Elapsed"
                         titleStyle={styles.listTitle}
                     />
                 </Surface>
