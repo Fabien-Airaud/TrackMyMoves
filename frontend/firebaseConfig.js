27a0d5910772cab87b432425a267e2992db1f8a2
FabienTri <airaudfabien@gmail.com>
2023-12-04 17:37:03 -0500

    Configs for firebase

--- frontend/firebaseConfig.js
+++ frontend/firebaseConfig.js
@@ -1,10 +1,12 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Optionally import the services that you want to use
-// import {...} from "firebase/auth";
 // import {...} from "firebase/database";
 // import {...} from "firebase/firestore";
 // import {...} from "firebase/functions";
@@ -13,17 +15,22 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD_qR_NVeVFJzq2jRr7pUI1DoAwzep5ofQ",
-    authDomain: "trackmymoves-a1b03.firebaseapp.com",
-    projectId: "trackmymoves-a1b03",
-    storageBucket: "trackmymoves-a1b03.appspot.com",
-    messagingSenderId: "17718062252",
-    appId: "1:17718062252:web:16b5737e7cb13946b84309",
-    measurementId: "G-6H828WN419"
-  };
+  apiKey: "AIzaSyD_qR_NVeVFJzq2jRr7pUI1DoAwzep5ofQ",
+  authDomain: "trackmymoves-a1b03.firebaseapp.com",
+  projectId: "trackmymoves-a1b03",
+  storageBucket: "trackmymoves-a1b03.appspot.com",
+  messagingSenderId: "17718062252",
+  appId: "1:17718062252:web:16b5737e7cb13946b84309",
+  measurementId: "G-6H828WN419"
+};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+// export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
