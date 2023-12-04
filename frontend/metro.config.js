ffb0e7c9f83b58dd87c14e855286104a5a1bc717
FabienTri <airaudfabien@gmail.com>
2023-12-04 16:42:31 -0500

    Correct metro config

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,8 +1,8 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-import { getDefaultConfig } from 'expo/metro-config';
+const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.sourceExts.push('cjs');
 
-export default defaultConfig;
+module.exports = defaultConfig;
