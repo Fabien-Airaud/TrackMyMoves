9a67fd3ca385a4b51872d286575d28b889ff79cf
FabienTri <airaudfabien@gmail.com>
2023-12-03 19:46:30 -0500

    Change to get activity from store

--- frontend/components/history/ActivityCharts.jsx
+++ frontend/components/history/ActivityCharts.jsx
@@ -1,7 +1,11 @@
 import { useTheme } from '@react-navigation/native';
 import { ScrollView, StyleSheet, Text } from 'react-native';
+import { useSelector } from 'react-redux';
 
-const ActivityCharts = ({ navigation, route: { params } }) => {
+const ActivityCharts = () => {
+    // Activity infos stored in redux
+    const activity = useSelector((state) => state.apiActivityInfos);
+
     // Style variables
     const { colors, fontSizes } = useTheme();
     const styles = StyleSheet.create({
@@ -18,7 +22,7 @@ const ActivityCharts = ({ navigation, route: { params } }) => {
 
     return (
         <ScrollView style={styles.page}>
-            <Text style={styles.text}>{JSON.stringify(params.sensorsIntervals)}</Text>
+            <Text style={styles.text}>{JSON.stringify(activity)}</Text>
         </ScrollView>
     );
 };
