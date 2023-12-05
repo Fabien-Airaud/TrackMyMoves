d081ff54edf440d553054d7b87782f5ac1e68028
FabienTri <airaudfabien@gmail.com>
2023-12-04 19:13:23 -0500

    Add firebase home stack when has a user logged in

--- frontend/components/navigation/AppNav.jsx
+++ frontend/components/navigation/AppNav.jsx
@@ -1,14 +1,31 @@
 import * as FileSystem from 'expo-file-system';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { onAuthStateChanged } from "firebase/auth";
 import { useDispatch, useSelector } from 'react-redux';
 
 import { newAccount } from '../../redux/apiAccountSlice';
 import { retrieveAccountAPI } from '../APIFunctions';
 import AppTab from './AppTab';
 import HomeStack from './HomeStack';
+import FirebaseStack from './FirebaseStack';
+import { auth } from '../../firebaseConfig';
 
 const AppNav = () => {
+    // Firebase user state
+    const [firebaseUser, setFirebaseUser] = useState(auth.currentUser);
+
     const dispatch = useDispatch();
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/auth.user
+          setFirebaseUser(user);
+        } else {
+          // User is signed out
+          setFirebaseUser(undefined);
+        }
+      });
 
     useEffect(() => {
         // log in if has a logIn file in local
@@ -33,7 +50,8 @@ const AppNav = () => {
     // Logged account stored in redux
     const loggedAccount = useSelector((state) => state.apiAccount);
 
-    if (loggedAccount.token) return <AppTab />;
+    if (firebaseUser) return <FirebaseStack />
+    else if (loggedAccount.token) return <AppTab />;
     else return <HomeStack />;
 };
 
