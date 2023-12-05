e9c475dc38e02a411fa3a8aefee28b6468e27bb2
FabienTri <airaudfabien@gmail.com>
2023-12-05 11:16:16 -0500

    Add register swagger

--- backend/track_my_moves/views.py
+++ backend/track_my_moves/views.py
@@ -123,6 +123,7 @@ def usersAdminStats(request, accountId):
 #   Partie API Rest
 ####################################################################################################
 
+@swagger_auto_schema(method="POST", request_body=AccountSerializer())
 @api_view(['POST'])
 def registerAPIViewDeco(request):
     serializer = AccountSerializer(data=request.data)
