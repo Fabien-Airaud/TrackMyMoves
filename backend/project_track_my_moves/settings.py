4e7ed62f578e4d0ff1aaa40c433524a644c348bc
FabienTri <airaudfabien@gmail.com>
2024-01-18 18:37:26 -0500

    Change localhost domain for expo

--- backend/project_track_my_moves/settings.py
+++ backend/project_track_my_moves/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-w6c*2hxg0fv1^zz6$*zhe5$-=7@#^n!!6txqpr#q&mer*k==ic
 DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.82']
 
 
 # Application definition
