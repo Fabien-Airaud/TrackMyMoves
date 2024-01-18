5e75ef549298d4ab1fd3318904a35cd937b344f8
FabienTri <airaudfabien@gmail.com>
2024-01-18 17:18:05 -0500

    Create AIModel page

---
+++ frontend/components/aiModel/AIModel.jsx
@@ -0,0 +1,24 @@
+import { StyleSheet, View, Text } from "react-native";
+
+const AIModel = () => {
+    // Style variables
+    const styles = StyleSheet.create({
+        page: {
+            height: '100%',
+            width: '100%',
+            alignItems: 'center',
+            justifyContent: 'center'
+        },
+        text: {
+            color: colors.text
+        }
+    });
+
+    return (
+        <View style={styles.page}>
+            <Text style={styles.text}>AI Model</Text>
+        </View>
+    );
+};
+
+export default AIModel;
