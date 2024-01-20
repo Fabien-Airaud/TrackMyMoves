65ca0302ebd46a8a168f0f70998729d8f9bdb5c9
FabienTri <airaudfabien@gmail.com>
2024-01-20 14:21:10 -0500

    Add Activity AI model

--- backend/track_my_moves/models/__init__.py
+++ backend/track_my_moves/models/__init__.py
@@ -1,5 +1,6 @@
 from .user import User
 from .account import Account
+from .activity_AI import ActivityAI
 from .activity import Activity
 from .activity_type import ActivityType
 from .activity_interval import ActivityInterval
