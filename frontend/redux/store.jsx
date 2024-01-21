d77b011f9a1120aea5fda3d732577e3d2ba743a6
FabienTri <airaudfabien@gmail.com>
2024-01-21 18:17:18 -0500

    Remove old redux slices

--- frontend/redux/store.jsx
+++ frontend/redux/store.jsx
@@ -1,17 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import accountReducer from './accountSlice';
 import apiAccountSlice from './apiAccountSlice';
-import apiActivitySlice from './apiActivitySlice';
-import currentActivitySlice from './currentActivitySlice';
-import logInReducer from './logInSlice';
 import apiActivityInfosSlice from './apiActivityInfosSlice';
+import apiActivitySlice from './apiActivitySlice';
 
 export default configureStore({
 	reducer: {
-		accounts: accountReducer,
-		logIn: logInReducer,
-		currentActivity: currentActivitySlice,
 		apiAccount: apiAccountSlice,
 		apiActivity: apiActivitySlice,
 		apiActivityInfos: apiActivityInfosSlice
