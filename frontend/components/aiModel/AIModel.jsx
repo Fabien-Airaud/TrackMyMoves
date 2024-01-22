e4ece047b0becad3d204b412e5844a5e1bb60464
FabienTri <airaudfabien@gmail.com>
2024-01-22 16:29:16 -0500

    Display report in a datatable

--- frontend/components/aiModel/AIModel.jsx
+++ frontend/components/aiModel/AIModel.jsx
@@ -1,11 +1,11 @@
 import { useTheme } from '@react-navigation/native';
 import { Button } from '@rneui/themed';
 import { useState } from 'react';
-import { Alert, ScrollView, StyleSheet, Text, View } from "react-native";
-import { useSelector } from 'react-redux';
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { DataTable } from 'react-native-paper';
+import { useSelector } from 'react-redux';
 
-import { retrieveModelTestsResultsAPI, testAIModelAPI, trainAIModelAPI } from '../APIFunctions';
+import { testAIModelAPI, trainAIModelAPI } from '../APIFunctions';
 
 const AIModel = () => {
     // State variables
@@ -57,18 +57,6 @@ const AIModel = () => {
         }
     });
 
-
-    // const getModelResults = () => {
-    //     retrieveModelTestsResultsAPI(apiAccount.token)
-    //         .then(response => {
-    //             if (response) setModelResults(response);
-    //             else Alert.alert(
-    //                 "Results error",
-    //                 "Can't get model results from server");
-    //         })
-    //         .catch(console.error);
-    // };
-
     const trainAIModel = () => {
         trainAIModelAPI(apiAccount.token, apiAccount.account.user.id)
             .then(response => {
@@ -79,12 +67,27 @@ const AIModel = () => {
             .catch(console.error);
     };
 
+    const changeReport = (report, caption) => {
+        let newReport = {}
+
+        for (const key in report) {
+            if (key == "accuracy") break;
+            newReport[caption[key]] = report[key]; // Replace id by label for each activityType
+        }
+
+        newReport["accuracy"] = { "precision": report.accuracy, "recall": report.accuracy, "f1-score": report.accuracy, "support": report.accuracy };
+        newReport["macro avg"] = report["macro avg"]
+        newReport["weighted avg"] = report["weighted avg"]
+        return newReport;
+    }
+
     const testAIModel = () => {
         testAIModelAPI(apiAccount.token, apiAccount.account.user.id)
             .then(response => {
                 if (response.result) {
+                    response.data.report = changeReport(response.data.report, response.data.caption);
                     setModelResults(response.data);
-                    console.log(JSON.stringify(modelResults))
+                    console.log(JSON.stringify(response.data));
                 }
                 else alert("Test failed", response.message);
             })
@@ -103,7 +106,26 @@ const AIModel = () => {
             {modelResults &&
                 <View style={styles.results}>
                     <Text style={styles.text}>Accuracy : {modelResults.accuracy * 100}%</Text>
-                    <Text style={styles.text}>Report : {JSON.stringify(modelResults.report)}</Text>
+                    <Text style={[styles.text, { marginBottom: -10 }]}>Report :</Text>
+                    <DataTable style={styles.datatable}>
+                        <DataTable.Header>
+                            <DataTable.Title style={styles.labelColumn} textStyle={{ color: colors.placeholder }}></DataTable.Title>
+                            <DataTable.Title style={styles.numericColumn} textStyle={{ color: colors.placeholder }} numeric>precision</DataTable.Title>
+                            <DataTable.Title style={styles.numericColumn} textStyle={{ color: colors.placeholder }} numeric>recall</DataTable.Title>
+                            <DataTable.Title style={styles.numericColumn} textStyle={{ color: colors.placeholder }} numeric>f1-score</DataTable.Title>
+                            <DataTable.Title style={styles.numericColumn} textStyle={{ color: colors.placeholder }} numeric>support</DataTable.Title>
+                        </DataTable.Header>
+
+                        {Object.entries(modelResults.report).map((rowData, index) => (
+                            <DataTable.Row key={index.toString()}>
+                                <DataTable.Cell style={styles.labelColumn} textStyle={{ color: colors.text }}>{rowData[0]}</DataTable.Cell>
+                                <DataTable.Cell style={styles.numericColumn} textStyle={{ color: colors.text }} numeric>{rowData[1]["precision"]}</DataTable.Cell>
+                                <DataTable.Cell style={styles.numericColumn} textStyle={{ color: colors.text }} numeric>{rowData[1]["recall"]}</DataTable.Cell>
+                                <DataTable.Cell style={styles.numericColumn} textStyle={{ color: colors.text }} numeric>{rowData[1]["f1-score"]}</DataTable.Cell>
+                                <DataTable.Cell style={styles.numericColumn} textStyle={{ color: colors.text }} numeric>{rowData[1]["support"]}</DataTable.Cell>
+                            </DataTable.Row>
+                        ))}
+                    </DataTable>
                     <Text style={styles.text}>Matrix : {JSON.stringify(modelResults.matrix)}</Text>
                     <Text style={styles.text}>Caption : {JSON.stringify(modelResults.caption)}</Text>
                 </View>
